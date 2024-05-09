14f3296d8823e632fe880401e0ef839c4356a109
aerooneqq <aerooneQ@yandex.ru>
2024-05-09 20:56:17 +0300

    Fix #3: associate bxes values with type ids

--- bxes/src/rust/bxes/src/models/domain/type_ids.rs
+++ bxes/src/rust/bxes/src/models/domain/type_ids.rs
@@ -1,4 +1,5 @@
 use variant_count::VariantCount;
+use crate::models::domain::bxes_value::BxesValue;
 
 #[derive(FromPrimitive, ToPrimitive, VariantCount, Debug, PartialEq, Eq)]
 pub enum TypeIds {
@@ -18,4 +19,25 @@ pub enum TypeIds {
     Drivers = 13,
     Guid = 14,
     SoftwareEventType = 15,
+}
+
+pub fn get_type_id(value: &BxesValue) -> TypeIds {
+    match value {
+        BxesValue::Null => TypeIds::Null,
+        BxesValue::Int32(_) => TypeIds::I32,
+        BxesValue::Int64(_) => TypeIds::I64,
+        BxesValue::Uint32(_) => TypeIds::U32,
+        BxesValue::Uint64(_) => TypeIds::U64,
+        BxesValue::Float32(_) => TypeIds::F32,
+        BxesValue::Float64(_) => TypeIds::F64,
+        BxesValue::String(_) => TypeIds::String,
+        BxesValue::Bool(_) => TypeIds::Bool,
+        BxesValue::Timestamp(_) => TypeIds::Timestamp,
+        BxesValue::BrafLifecycle(_) => TypeIds::BrafLifecycle,
+        BxesValue::StandardLifecycle(_) => TypeIds::StandardLifecycle,
+        BxesValue::Artifact(_) => TypeIds::Artifact,
+        BxesValue::Drivers(_) => TypeIds::Drivers,
+        BxesValue::Guid(_) => TypeIds::Guid,
+        BxesValue::SoftwareEventType(_) => TypeIds::SoftwareEventType,
+    }
 }
