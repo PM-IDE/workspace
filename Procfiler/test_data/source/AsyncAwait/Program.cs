cb7903278098bd01263a930fa411a452da8ec33c
aerooneqq <aerooneQ@yandex.ru>
2024-08-05 22:27:17 +0300

    Add new test data for async methods

---
+++ Procfiler/test_data/source/AsyncAwait/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace AsyncAwait;
+
+public class Program
+{
+  public static async Task Main()
+  {
+    await Method1();
+    await Method2();
+
+    await Method3();
+    
+    await Method2();
+    
+    await Method3();
+  }
+
+  static async Task<int> Method1()
+  {
+    var x = new Method1();
+    await Task.Delay(1000);
+    x = new Method1();
+    return 1;
+  }
+
+  static async Task Method2()
+  {
+    var x = new Method2();
+    var xd = await Method1();
+    x = new Method2();
+  }
+
+  static async Task Method3()
+  {
+    var xd = new Method3();
+  }
+}
+
+class Method1;
+
+class Method2;
+
+class Method3;
---
+++ Procfiler/test_data/source/AsyncAwaitTaskFactoryNew/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace AsyncAwaitTaskFactoryNew;
+
+public static class Program
+{
+  public static async Task Main()
+  {
+    await Task.Factory.StartNew(() =>
+    {
+      var xd = new XdClass1();
+    });
+
+    await Task.Factory.StartNew(async () =>
+    {
+      var xd = new XdClass2();
+      await Task.Delay(100);
+    });
+    
+    await Task.Factory.StartNew(() =>
+    {
+      var xd = new XdClass3();
+    });
+    
+    Thread.Sleep(1000);
+  }
+}
+
+class XdClass1;
+
+class XdClass2;
+
+class XdClass3;
