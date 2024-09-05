9beb7350144fb9b27cda0cdd1976da1d9ca82fc2
aerooneqq <aerooneQ@yandex.ru>
2024-09-05 20:08:35 +0300

    WIP

--- Ficus/src/python/tests/grpc_pipelines/kafka_pipeline_tests.py
+++ Ficus/src/python/tests/grpc_pipelines/kafka_pipeline_tests.py
@@ -14,3 +14,18 @@ def test_kafka_pipeline():
     KafkaPipeline(
         PrintEventLogInfo()
     ).execute(kafka_metadata, {})
+
+
+def test_kafka_stream_pipeline():
+    kafka_metadata = KafkaPipelineMetadata(
+        topic_name="my-topic",
+        kafka_consumer_configuration={
+            'bootstrap.servers': 'localhost:9092',
+            'group.id': 'xd',
+            'auto.offset.reset': 'earliest'
+        }
+    )
+
+    KafkaPipeline(
+        PrintEventLogInfo()
+    ).execute_stream(kafka_metadata, {})
