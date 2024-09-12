b87ff85b0a949a7da71eac81fa843dcf018eb0c3
aero <aerooneQ@yandex.ru>
2024-09-12 16:08:11 +0300

    WIP

--- Ficus/src/front/FicusFrontend/FicusFrontend/Program.cs
+++ Ficus/src/front/FicusFrontend/FicusFrontend/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using FicusFrontend;
+using FicusFrontend.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<ICasesService, CasesService>();
 
 await builder.Build().RunAsync();
---
+++ Ficus/src/front/FicusFrontend/FicusFrontend/Services/ICasesService.cs
@@ -0,0 +1,32 @@
+﻿namespace FicusFrontend.Services;
+
+public class Case
+{
+  public required string Name { get; init; }
+}
+
+public interface ICasesService
+{
+  IAsyncEnumerable<Case> OpenCasesStream();
+}
+
+public class CasesService : ICasesService
+{
+  private readonly List<Case> myCases =
+  [
+    new() { Name = "Case 1" },
+    new() { Name = "Case 2" },
+    new() { Name = "Case 3" },
+    new() { Name = "Case 4" }
+  ];
+  
+
+  public async IAsyncEnumerable<Case> OpenCasesStream()
+  {
+    foreach (var @case in myCases)
+    {
+      yield return @case;
+      await Task.Delay(1000);
+    }
+  }
+}
