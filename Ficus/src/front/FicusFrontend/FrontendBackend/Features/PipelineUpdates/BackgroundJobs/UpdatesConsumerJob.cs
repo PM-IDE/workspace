d8eb9007261d117a82471d1e8f74d9c346460305
aerooneqq <aerooneQ@yandex.ru>
2024-09-15 00:27:56 +0300

    Fix background job

--- Ficus/src/front/FicusFrontend/FrontendBackend/Features/PipelineUpdates/BackgroundJobs/UpdatesConsumerJob.cs
+++ Ficus/src/front/FicusFrontend/FrontendBackend/Features/PipelineUpdates/BackgroundJobs/UpdatesConsumerJob.cs
@@ -1,16 +1,26 @@
 ﻿using FrontendBackend.Features.PipelineUpdates.Kafka.PipelineUpdates;
+using Google.Protobuf;
 
 namespace FrontendBackend.Features.PipelineUpdates.BackgroundJobs;
 
-public class UpdatesConsumerJob(IPipelinePartsUpdatesConsumer consumer, ILogger<UpdatesConsumerJob> logger) : BackgroundService
+public class UpdatesConsumerJob(IPipelinePartsUpdatesConsumer consumer, ILogger<UpdatesConsumerJob> logger) : IHostedService
 {
-  protected override Task ExecuteAsync(CancellationToken stoppingToken)
+  public Task StartAsync(CancellationToken cancellationToken)
   {
-    consumer.StartUpdatesConsuming(stoppingToken, _ =>
-    {
-      logger.LogInformation("Update");
-    });
-
+    Task.Factory.StartNew(() => ExecuteConsumerRoutine(cancellationToken), cancellationToken);
     return Task.CompletedTask;
+  }
+
+  public Task StopAsync(CancellationToken cancellationToken)
+  {
+    return Task.CompletedTask;
+  }
+  
+  private void ExecuteConsumerRoutine(CancellationToken stoppingToken)
+  {
+    consumer.StartUpdatesConsuming(stoppingToken, update =>
+    {
+      logger.LogInformation("Update {Update}", update.ToByteString().ToString());
+    });
   }
 }
