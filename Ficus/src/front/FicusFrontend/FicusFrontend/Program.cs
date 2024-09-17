b8220369655dc49afdc082d2ce248ee64fbe6c08
aero <aerooneQ@yandex.ru>
2024-09-17 16:39:56 +0300

    Add settings, remove not need file

--- Ficus/src/front/FicusFrontend/FicusFrontend/Program.cs
+++ Ficus/src/front/FicusFrontend/FicusFrontend/Program.cs
@@ -5,17 +5,22 @@ using Grpc.Net.Client;
 using Grpc.Net.Client.Web;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Options;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddSingleton<ICasesService, CasesService>();
+builder.Services.Configure<ApplicationSettings>(builder.Configuration.GetSection(nameof(ApplicationSettings)));
 
-builder.Services.AddSingleton(_ =>
+builder.Services.AddSingleton(services =>
 {
+  var settings = services.GetRequiredService<IOptions<ApplicationSettings>>().Value;
   var httpHandler = new GrpcWebHandler(GrpcWebMode.GrpcWebText, new HttpClientHandler());
-  var channel = GrpcChannel.ForAddress("http://localhost:5122", new GrpcChannelOptions { HttpHandler = httpHandler });
+
+  var channel = GrpcChannel.ForAddress(settings.BackendUrl, new GrpcChannelOptions { HttpHandler = httpHandler });
+
   return new GrpcPipelinePartsContextValuesService.GrpcPipelinePartsContextValuesServiceClient(channel);
 });
 
