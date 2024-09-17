4d66371a52b8f8306a25db34aca82ef2284d60db
aerooneqq <aerooneQ@yandex.ru>
2024-09-17 11:42:18 +0300

    Fix grpc connection

--- Ficus/src/front/FicusFrontend/FicusFrontend/Program.cs
+++ Ficus/src/front/FicusFrontend/FicusFrontend/Program.cs
@@ -2,6 +2,7 @@ using Ficus;
 using FicusFrontend;
 using FicusFrontend.Services;
 using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -13,7 +14,8 @@ builder.Services.AddSingleton<ICasesService, CasesService>();
 
 builder.Services.AddSingleton(_ =>
 {
-  var channel = GrpcChannel.ForAddress("localhost:5122");
+  var httpHandler = new GrpcWebHandler(GrpcWebMode.GrpcWebText, new HttpClientHandler());
+  var channel = GrpcChannel.ForAddress("http://localhost:5122", new GrpcChannelOptions { HttpHandler = httpHandler });
   return new GrpcPipelinePartsContextValuesService.GrpcPipelinePartsContextValuesServiceClient(channel);
 });
 
