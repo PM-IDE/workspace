2be587faa131fd27bf242de7951f4defb5c2b42d
aero <aerooneQ@yandex.ru>
2024-10-04 17:24:31 +0300

    WIP on drawing petri nets

--- Ficus/src/front/FicusFrontend/FicusFrontend/Components/CaseInfo/ContextValues/PetriNet/PetriNetRenderer.razor.js
+++ Ficus/src/front/FicusFrontend/FicusFrontend/Components/CaseInfo/ContextValues/PetriNet/PetriNetRenderer.razor.js
@@ -1,38 +1,70 @@
 window.drawPetriNet = function (id, net) {
   let element = document.getElementById(id);
+  let elements = [];
+
+  for (const place of net.places) {
+    elements.push({
+      data: {
+        id: place.id.toString()
+      }
+    });
+  }
+  
+  for (const transition of net.transitions) {
+    elements.push({
+      data: {
+        id: transition.id.toString()
+      }
+    });
+  }
+  
+  for (const transition of net.transitions) {
+    for (const arc of transition.incomingArcs) {
+      elements.push({
+        data: {
+          id: arc.placeId.toString() + "::" + transition.id.toString(),
+          source: arc.placeId.toString(),
+          target: transition.id.toString(),
+        }
+      });
+    }
+    
+    for (const arc of transition.outgoingArcs) {
+      elements.push({
+        data: {
+          id: transition.id.toString() + "::" + arc.placeId.toString(),
+          target: arc.placeId.toString(),
+          source: transition.id.toString(),
+        }
+      })
+    }
+  }
+  
+  let nodeStyle = {
+    selector: 'node',
+    style: {
+      'background-color': '#666',
+      'label': 'data(id)'
+    }
+  };
+  
+  let edgeStyle = {
+    selector: 'edge',
+    style: {
+      'width': 3,
+      'line-color': '#ccc',
+      'target-arrow-color': '#ccc',
+      'target-arrow-shape': 'triangle',
+      'curve-style': 'bezier'
+    }
+  };
+
   let cy = cytoscape({
     container: element,
-    elements: [
-      {
-        data: { id: 'a' }
-      },
-      {
-        data: { id: 'b' }
-      },
-      {
-        data: { id: 'ab', source: 'a', target: 'b' }
-      }
-    ],
-
+    elements: elements,
     style: [
-      {
-        selector: 'node',
-        style: {
-          'background-color': '#666',
-          'label': 'data(id)'
-        }
-      },
-
-      {
-        selector: 'edge',
-        style: {
-          'width': 3,
-          'line-color': '#ccc',
-          'target-arrow-color': '#ccc',
-          'target-arrow-shape': 'triangle',
-          'curve-style': 'bezier'
-        }
-      }
+      nodeStyle,
+      edgeStyle
     ],
 
     layout: {
