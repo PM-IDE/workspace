00ac996b43e4fddf45e9bfd2e94747183f22d311
aero <aerooneQ@yandex.ru>
2024-10-05 17:50:09 +0300

    Meaningless refactorings

--- Ficus/src/front/FicusFrontend/FicusFrontend/Npm/src/petri_net_renderer.js
+++ Ficus/src/front/FicusFrontend/FicusFrontend/Npm/src/petri_net_renderer.js
@@ -3,98 +3,114 @@ import klay from 'cytoscape-klay';
 
 export default set_draw_petri_net;
 
+const placeType = "place";
+const transitionType = "transition";
+const arcType = "arc";
+
 function set_draw_petri_net() {
   window.drawPetriNet = function (id, net) {
-    let element = document.getElementById(id);
-    let elements = [];
-
-    const placeType = "place";
-    const transitionType = "transition";
-    const arcType = "arc";
-
-    for (const place of net.places) {
-      elements.push({
-        data: {
-          type: placeType,
-          id: place.id.toString()
-        }
-      });
-    }
-
-    for (const transition of net.transitions) {
-      elements.push({
-        data: {
-          type: transitionType,
-          id: transition.id.toString(),
-          name: transition.data
-        }
-      });
-    }
-
-    for (const transition of net.transitions) {
-      for (const arc of transition.incomingArcs) {
-        elements.push({
-          data: {
-            type: arcType,
-            id: arc.placeId.toString() + "::" + transition.id.toString(),
-            source: arc.placeId.toString(),
-            target: transition.id.toString(),
-          }
-        });
-      }
-
-      for (const arc of transition.outgoingArcs) {
-        elements.push({
-          data: {
-            type: arcType,
-            id: transition.id.toString() + "::" + arc.placeId.toString(),
-            target: arc.placeId.toString(),
-            source: transition.id.toString(),
-          }
-        })
-      }
-    }
-
-    let nodeStyle = {
-      selector: 'node',
-      style: {
-        'background-color': '#666'
-      }
-    };
-
-    let transitionNodeStyle = {
-      selector: `node[type="${transitionType}"]`,
-      style: {
-        'shape': 'rectangle',
-        'label': 'data(name)'
-      }
-    }
-
-    let edgeStyle = {
-      selector: 'edge',
-      style: {
-        'width': 3,
-        'line-color': '#ccc',
-        'target-arrow-color': '#ccc',
-        'target-arrow-shape': 'triangle',
-        'curve-style': 'bezier'
-      }
-    };
-
     cytoscape.use(klay);
+    cytoscape(createCytoscapeOptions(id, net));
+  } 
+}
 
-    let cy = cytoscape({
-      container: element,
-      elements: elements,
-      style: [
-        nodeStyle,
-        transitionNodeStyle,
-        edgeStyle
-      ],
+function createCytoscapeOptions(id, net) {
+  return {
+    container: document.getElementById(id),
+    elements: createElementsFromNet(net),
+    style: createStylesList(),
 
-      layout: {
-        name: 'klay',
+    layout: {
+      name: 'klay',
+    }
+  }
+}
+
+function createElementsFromNet(net) {
+  const elements = [];
+
+  for (const place of net.places) {
+    elements.push({
+      data: {
+        type: placeType,
+        id: place.id.toString()
       }
     });
-  } 
+  }
+
+  for (const transition of net.transitions) {
+    elements.push({
+      data: {
+        type: transitionType,
+        id: transition.id.toString(),
+        name: transition.data
+      }
+    });
+  }
+
+  for (const transition of net.transitions) {
+    for (const arc of transition.incomingArcs) {
+      elements.push({
+        data: {
+          type: arcType,
+          id: arc.placeId.toString() + "::" + transition.id.toString(),
+          source: arc.placeId.toString(),
+          target: transition.id.toString(),
+        }
+      });
+    }
+
+    for (const arc of transition.outgoingArcs) {
+      elements.push({
+        data: {
+          type: arcType,
+          id: transition.id.toString() + "::" + arc.placeId.toString(),
+          target: arc.placeId.toString(),
+          source: transition.id.toString(),
+        }
+      })
+    }
+  }
+  
+  return elements;
+}
+
+function createStylesList() {
+  return [
+    createCommonNodeStyle(),
+    createTransitionNodeStyle(),
+    createEdgeStyle()
+  ];
+}
+
+function createCommonNodeStyle() {
+  return {
+    selector: 'node',
+    style: {
+      'background-color': '#666'
+    }
+  };
+}
+
+function createTransitionNodeStyle() {
+  return {
+    selector: `node[type="${transitionType}"]`,
+    style: {
+      'shape': 'rectangle',
+      'label': 'data(name)'
+    }
+  };
+}
+
+function createEdgeStyle() {
+  return {
+    selector: 'edge',
+    style: {
+      'width': 3,
+      'line-color': '#ccc',
+      'target-arrow-color': '#ccc',
+      'target-arrow-shape': 'triangle',
+      'curve-style': 'bezier'
+    }
+  };
 }
