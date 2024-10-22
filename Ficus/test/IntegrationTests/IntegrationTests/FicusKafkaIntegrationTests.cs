57874b69a1d24969c6e03e52a6cceee3da693526
aero <aerooneQ@yandex.ru>
2024-10-22 09:16:36 +0300

    WIP

--- Ficus/test/IntegrationTests/IntegrationTests/FicusKafkaIntegrationTests.cs
+++ Ficus/test/IntegrationTests/IntegrationTests/FicusKafkaIntegrationTests.cs
@@ -1,3 +1,41 @@
+using Bxes.Kafka;
+using Bxes.Models.Domain;
+using Bxes.Models.System;
+using Bxes.Utils;
+using Confluent.Kafka;
+
 namespace IntegrationTests;
 
-public class FicusKafkaIntegrationTests;
+internal static class EnvVars
+{
+  public const string BootstrapServers = nameof(BootstrapServers);
+  public const string TopicName = nameof(TopicName);
+
+  public static string GetEnvOrThrow(string envVarName) => Environment.GetEnvironmentVariable(envVarName) ??
+                                                           throw new Exception($"Env var {envVarName} is not set");
+} 
+
+public class FicusKafkaIntegrationTests
+{
+  [Test]
+  public void DoTest()
+  {
+    var eventLog = RandomLogsGenerator.CreateSimpleLog();
+    var writer = CreateBxesKafkaWriter();
+    foreach (var @event in eventLog.ToEventsStream())
+    {
+      writer.HandleEvent(@event);
+    }
+    
+    
+  }
+
+  private BxesKafkaStreamWriter<IEvent> CreateBxesKafkaWriter() => new(
+    new SystemMetadata(),
+    EnvVars.GetEnvOrThrow(EnvVars.TopicName),
+    new ProducerConfig
+    {
+      BootstrapServers = EnvVars.GetEnvOrThrow(EnvVars.BootstrapServers)
+    }
+  );
+}
