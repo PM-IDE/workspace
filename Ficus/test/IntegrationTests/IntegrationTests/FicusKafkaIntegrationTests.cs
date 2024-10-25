5b063ddce9e323359212cf32313b52084a9c429d
aero <aerooneQ@yandex.ru>
2024-10-25 10:11:56 +0300

    WIP on tests

--- Ficus/test/IntegrationTests/IntegrationTests/FicusKafkaIntegrationTests.cs
+++ Ficus/test/IntegrationTests/IntegrationTests/FicusKafkaIntegrationTests.cs
@@ -3,39 +3,55 @@ using Bxes.Models.Domain;
 using Bxes.Models.System;
 using Bxes.Utils;
 using Confluent.Kafka;
+using FicusKafkaIntegration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.EnvironmentVariables;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace IntegrationTests;
 
-internal static class EnvVars
-{
-  public const string BootstrapServers = nameof(BootstrapServers);
-  public const string TopicName = nameof(TopicName);
 
-  public static string GetEnvOrThrow(string envVarName) => Environment.GetEnvironmentVariable(envVarName) ??
-                                                           throw new Exception($"Env var {envVarName} is not set");
-} 
+public class FicusKafkaProducerSettings
+{
+  public required string Topic { get; init; }
+  public required string BootstrapServers { get; init; }
+}
 
 public class FicusKafkaIntegrationTests
 {
   [Test]
   public void DoTest()
   {
+    var configuration = new ConfigurationBuilder().Add(new EnvironmentVariablesConfigurationSource()).Build();
+    var producerSettings = configuration.GetSection(nameof(FicusKafkaProducerSettings)).Get<FicusKafkaProducerSettings>()!;
+
     var eventLog = RandomLogsGenerator.CreateSimpleLog();
-    var writer = CreateBxesKafkaWriter();
+    var writer = CreateBxesKafkaWriter(producerSettings);
     foreach (var @event in eventLog.ToEventsStream())
     {
       writer.HandleEvent(@event);
     }
-    
-    
+
+    var logger = LoggerFactory.Create(_ => { }).CreateLogger<PipelinePartsUpdatesConsumer>();
+    var pipelinePartsConsumerSettings = configuration
+      .GetSection(nameof(PipelinePartsUpdateKafkaSettings))
+      .Get<PipelinePartsUpdateKafkaSettings>()!;
+
+    var consumer = new PipelinePartsUpdatesConsumer(Options.Create(pipelinePartsConsumerSettings), logger);
+
+    foreach (var update in consumer.StartUpdatesConsuming(CancellationToken.None))
+    {
+      Console.WriteLine(update.ProcessCaseMetadata);
+    }
   }
 
-  private BxesKafkaStreamWriter<IEvent> CreateBxesKafkaWriter() => new(
+  private BxesKafkaStreamWriter<IEvent> CreateBxesKafkaWriter(FicusKafkaProducerSettings settings) => new(
     new SystemMetadata(),
-    EnvVars.GetEnvOrThrow(EnvVars.TopicName),
+    settings.Topic,
     new ProducerConfig
     {
-      BootstrapServers = EnvVars.GetEnvOrThrow(EnvVars.BootstrapServers)
+      BootstrapServers = settings.BootstrapServers
     }
   );
 }
