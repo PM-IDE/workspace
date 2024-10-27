0045ee308c9283dc605d1bbb1d6bc92738e2f70a
aerooneqq <aerooneQ@yandex.ru>
2024-10-27 20:44:36 +0300

    Refactor pipelines examples

--- Ficus/examples/kafka_pipelines/kafka_pipeline.py
+++ Ficus/examples/kafka_pipelines/kafka_pipeline.py
@@ -1,43 +1,14 @@
+from common import execute_pipeline
 from ficus import *
-import os
 
-def env_or_default(env_name: str, default: str):
-    env = os.getenv(env_name)
-    return env if env is not None else default
-
-consumer_servers = env_or_default('CONSUMER_BOOTSTRAP_SERVERS', 'localhost:9092')
-consumer_topic = env_or_default('CONSUMER_TOPIC', 'my-topic')
-consumer_group = env_or_default('CONSUMER_GROUP_ID', 'xd')
-
-kafka_consumer_metadata = KafkaPipelineMetadata(
-    topic_name=consumer_topic,
-    kafka_consumer_configuration={
-        'bootstrap.servers': consumer_servers,
-        'group.id': consumer_group,
-        'auto.offset.reset': 'earliest'
-    }
+execute_pipeline(
+    [
+        PrintEventLogInfo(),
+        TracesDiversityDiagramCanvas(),
+        DiscoverPetriNetHeuristic(),
+        EnsureInitialMarking(),
+        AnnotatePetriNetWithFrequency(),
+        DiscoverFuzzyGraph(),
+        ViewGraph()
+    ]
 )
-
-producer_servers = env_or_default('PRODUCER_BOOTSTRAP_SERVERS', 'localhost:9092')
-producer_topic = env_or_default('PRODUCER_TOPIC', 'ficus-topic')
-
-kafka_producer_metadata = KafkaPipelineMetadata(
-    topic_name=producer_topic,
-    kafka_consumer_configuration={
-        'bootstrap.servers': producer_servers
-    }
-)
-
-ficus_backend = env_or_default('FICUS_BACKEND', 'localhost:8080')
-
-KafkaPipeline(
-    PrintEventLogInfo(),
-    TracesDiversityDiagramCanvas(),
-    DiscoverPetriNetHeuristic(),
-    EnsureInitialMarking(),
-    AnnotatePetriNetWithFrequency(),
-    DiscoverFuzzyGraph(),
-    ViewGraph()
-).execute(kafka_consumer_metadata, kafka_producer_metadata, {
-    ficus_backend_addr_key: ficus_backend
-})
