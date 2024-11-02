6bfe1b0363500d2fdc825bea14845087a05dac20
aerooneqq <aerooneQ@yandex.ru>
2024-11-02 22:48:58 +0300

    Supporting GC traces

---
+++ Procfiler/src/dotnet/ProcfilerOnline/Core/Handlers/GcHandler.cs
@@ -0,0 +1,21 @@
+﻿using Core.Container;
+using Core.Events.EventRecord;
+using ProcfilerOnline.Core.Features;
+
+namespace ProcfilerOnline.Core.Handlers;
+
+public class GcEvent : IEventPipeStreamEvent
+{
+  public required List<EventRecordWithMetadata> GcTrace { get; init; }
+}
+
+[AppComponent]
+public class GcHandler : IEventPipeStreamEventHandler
+{
+  public void Handle(IEventPipeStreamEvent eventPipeStreamEvent)
+  {
+    if (!ProcfilerOnlineFeatures.ProduceEventsToKafka.IsEnabled()) return;
+
+    Console.WriteLine("Handled a completed GC");
+  }
+}
---
+++ Procfiler/src/dotnet/ProcfilerOnline/Core/Processors/GcEventsProcessor.cs
@@ -0,0 +1,43 @@
+﻿using Core.Constants.TraceEvents;
+using Core.Container;
+using Core.Events.EventRecord;
+using ProcfilerOnline.Core.Handlers;
+
+namespace ProcfilerOnline.Core.Processors;
+
+[AppComponent]
+public class GcEventsProcessor(ICompositeEventPipeStreamEventHandler handler) : ITraceEventProcessor
+{
+  private int myGcCount;
+  private readonly List<EventRecordWithMetadata> myCurrentGcTrace = [];
+
+  public void Process(EventProcessingContext context)
+  {
+    myCurrentGcTrace.Add(context.Event);
+
+    switch (context.Event.EventClass)
+    {
+      case TraceEventsConstants.GcStart:
+      {
+        myGcCount++;
+        break;
+      }
+      case TraceEventsConstants.GcStop:
+      {
+        myGcCount--;
+        if (myGcCount == 0)
+        {
+          var gcTrace = myCurrentGcTrace.ToList();
+          myCurrentGcTrace.Clear();
+
+          handler.Handle(new GcEvent
+          {
+            GcTrace = gcTrace
+          });
+        }
+
+        break;
+      }
+    }
+  }
+}
