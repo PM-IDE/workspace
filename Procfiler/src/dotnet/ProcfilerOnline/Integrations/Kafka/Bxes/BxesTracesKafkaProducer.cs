fee10beb8ae2ef09a98283cf6a9fd4354fea44c0
aerooneqq <aerooneQ@yandex.ru>
2024-11-03 16:23:12 +0300

    More clear error logging when failing to produce to kafka

--- Procfiler/src/dotnet/ProcfilerOnline/Integrations/Kafka/Bxes/BxesTracesKafkaProducer.cs
+++ Procfiler/src/dotnet/ProcfilerOnline/Integrations/Kafka/Bxes/BxesTracesKafkaProducer.cs
@@ -6,6 +6,8 @@ using Confluent.Kafka;
 using Core.Bxes;
 using Core.Container;
 using Core.Events.EventRecord;
+using Core.Utils;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using ProcfilerOnline.Core.Settings;
 
@@ -23,7 +25,7 @@ public class BxesKafkaTrace
 }
 
 [AppComponent]
-public class BxesTracesKafkaProducer(IOptions<OnlineProcfilerSettings> settings) : IBxesMethodsKafkaProducer
+public class BxesTracesKafkaProducer(IOptions<OnlineProcfilerSettings> settings, IProcfilerLogger logger) : IBxesMethodsKafkaProducer
 {
   private readonly IBxesStreamWriter myWriter = new BxesKafkaStreamWriter<BxesEvent>(
     BxesUtil.CreateSystemMetadata(),
@@ -44,13 +46,26 @@ public class BxesTracesKafkaProducer(IOptions<OnlineProcfilerSettings> settings)
 
     metadata.AddRange(message.Metadata);
 
-    myWriter.HandleEvent(new BxesTraceVariantStartEvent(1, metadata));
-
-    foreach (var eventRecord in message.Trace)
+    try
     {
-      myWriter.HandleEvent(new BxesEventEvent<BxesEvent>(new BxesEvent(eventRecord, true)));
-    }
+      myWriter.HandleEvent(new BxesTraceVariantStartEvent(1, metadata));
 
-    myWriter.HandleEvent(BxesKafkaTraceVariantEndEvent.Instance);
+      foreach (var eventRecord in message.Trace)
+      {
+        myWriter.HandleEvent(new BxesEventEvent<BxesEvent>(new BxesEvent(eventRecord, true)));
+      }
+
+      myWriter.HandleEvent(BxesKafkaTraceVariantEndEvent.Instance);
+    }
+    catch (Exception ex)
+    {
+      logger.LogError(
+        ex,
+        "Failed to produce bXES trace to kafka, process name: {ProcessName}, case name: {CaseName}, events count: {EventsCount}",
+        message.ProcessName,
+        message.CaseName,
+        message.Trace.Count
+      );
+    }
   }
 }
