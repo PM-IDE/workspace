9b7b39c067fee389c2a4f281409e855ef79c0b55
aero <aerooneQ@yandex.ru>
2024-11-28 01:02:23 +0300

    Add tests for business procfiler logging

---
+++ Procfiler/src/dotnet/ProcfilerTests/Tests/ProcfilerEventPipeLoggerTests/ProcfilerEventPipeLoggerTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+using ProcfilerTests.Core;
+using TestsUtil;
+
+namespace ProcfilerTests.Tests.ProcfilerEventPipeLoggerTests;
+
+public class ProcfilerEventPipeLoggerTest : GoldProcessBasedTest
+{
+  [Test]
+  public void DoTest()
+  {
+    ExecuteTestWithGold(KnownSolution.ProcfilerEventPipeLogger.CreateDefaultContext(), events =>
+    {
+      return string.Join(
+        "\n", 
+        events.Events
+          .Where(e => e.Event.EventName is "BusinessEvent")
+          .Select(e => e.Event)
+          .OrderBy(e => e.Time.QpcStamp)
+          .Select(e => $"{e.EventName} {JsonSerializer.Serialize(e.Metadata)}")
+      );
+    });
+  }
+}
