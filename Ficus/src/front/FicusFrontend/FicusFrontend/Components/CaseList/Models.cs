1452b2e35433b6825a3882a480bd8edcf50ce264
aerooneqq <aerooneQ@yandex.ru>
2024-12-06 13:38:24 +0300

    Creating common sidebar list

---
+++ Ficus/src/front/FicusFrontend/FicusFrontend/Components/CaseList/Models.cs
@@ -0,0 +1,30 @@
+﻿namespace FicusFrontend.Components.CaseList;
+
+public class CollectionItemInfo<TItem, TInnerItem, TId> where TId : notnull
+{
+  public required TId Id { get; init; }
+  public required string Name { get; init; }
+  public required TItem Item { get; init; }
+  public required List<InnerCollectionItemInfo<TInnerItem, TId>> InnerItems { get; init; }
+}
+
+public class InnerCollectionItemInfo<TCollectionInnerItem, TId> where TId : notnull
+{
+  public required TId Id { get; init; }
+  public required TCollectionInnerItem InnerItem { get; init; }
+  public required ListItemInfo ListItemInfo { get; init; }
+}
+
+public class ListItemInfo
+{
+  public required DateTime UpdatedAt { get; init; }
+  public required string Name { get; init; }
+  public required ItemProcessingState ProcessingState { get; set; }
+}
+
+public enum ItemProcessingState
+{
+  New,
+  Updated,
+  Seen
+}
