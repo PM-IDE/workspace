a6292656fe46ae78d2ec753e2f21a120dde4db40
aerooneqq <aerooneq@yandex.ru>
2025-01-13 22:04:14 +0300

    WIP on streaming algorithms

--- Ficus/src/rust/ficus/src/features/streaming/lossy_count.rs
+++ Ficus/src/rust/ficus/src/features/streaming/lossy_count.rs
@@ -1,12 +1,23 @@
 use std::collections::HashMap;
 use std::hash::Hash;
 
-struct State {
+#[derive(Debug)]
+pub struct Entry<T> {
+    key: T,
+    approx_frequency: f64,
+}
+
+impl<T> Entry<T> {
+    pub fn key(&self) -> &T { &self.key }
+    pub fn approx_frequency(&self) -> f64 { self.approx_frequency }
+}
+
+struct LossyCountState {
     freq: u64,
     delta: f64,
 }
 
-impl State {
+impl LossyCountState {
     pub fn new(delta: f64) -> Self {
         Self {
             freq: 1,
@@ -16,7 +27,7 @@ impl State {
 }
 
 pub struct LossyCount<T> where T: Hash + Eq {
-    state: HashMap<T, State>,
+    state: HashMap<T, LossyCountState>,
     batch_size: u64,
     observed_items_count: u64
 }
@@ -30,18 +41,33 @@ impl<T> LossyCount<T> where T: Hash + Eq + Clone {
         }
     }
 
+    pub fn state(&self) -> Vec<Entry<T>> {
+        self.above_threshold(0f64)
+    }
+
+    pub fn above_threshold(&self, threshold: f64) -> Vec<Entry<T>> {
+        self.state
+            .iter()
+            .filter(|s| s.1.freq as f64 >= (threshold - s.1.delta) * (self.observed_items_count as f64))
+            .map(|s| Entry {
+                key: s.0.clone(),
+                approx_frequency: s.1.freq as f64 / (self.observed_items_count as f64)
+            })
+            .collect()
+    }
+
     pub fn observe(&mut self, e: T) {
         self.observed_items_count += 1;
-        let bucket_number = self.observed_items_count as f64 / self.batch_size as f64;
+        let bucket_number = self.observed_items_count / self.batch_size + 1;
 
         if self.state.contains_key(&e) {
             self.state.get_mut(&e).unwrap().freq += 1;
         } else {
-            self.state.insert(e, State::new(bucket_number));
+            self.state.insert(e, LossyCountState::new((bucket_number - 1) as f64));
         }
 
         if self.observed_items_count % self.batch_size == 0 {
-            self.prune(bucket_number);
+            self.prune(bucket_number as f64);
         }
     }
 
