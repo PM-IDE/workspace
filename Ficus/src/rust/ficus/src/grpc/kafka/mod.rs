d69787db7ca9a09ef45589957c9f9a313cfe2b72
aerooneqq <aerooneQ@yandex.ru>
2025-01-22 17:48:35 +0300

    Adding streaming configuration

--- Ficus/src/rust/ficus/src/grpc/kafka/mod.rs
+++ Ficus/src/rust/ficus/src/grpc/kafka/mod.rs
@@ -1,3 +1,4 @@
 pub mod grpc_kafka_service;
 pub mod kafka_service;
 pub mod models;
+pub mod streaming_configs;
---
+++ Ficus/src/rust/ficus/src/grpc/kafka/streaming_configs.rs
@@ -0,0 +1,88 @@
+use crate::ficus_proto::{GrpcPipelineStreamingConfiguration, GrpcT1EventsTimeBasedCaching, GrpcT1StreamingConfiguration, GrpcT1TraceTimeBasedCaching, GrpcT2LossyCountConfiguration, GrpcT2StreamingConfiguration};
+
+type StreamingConfigurationEnum = crate::ficus_proto::grpc_pipeline_streaming_configuration::Configuration;
+type T1ConfigurationEnum = crate::ficus_proto::grpc_t1_streaming_configuration::Configuration;
+type T2ConfigurationEnum = crate::ficus_proto::grpc_t2_streaming_configuration::Configuration;
+
+
+enum StreamingConfiguration {
+    NotSpecified,
+    T1(T1StreamingConfiguration),
+    T2(T2StreamingConfiguration)
+}
+
+impl StreamingConfiguration {
+    pub fn new(grpc_config: &GrpcPipelineStreamingConfiguration) -> Option<Self> {
+        match grpc_config.configuration.as_ref() {
+            None => None,
+            Some(c) => match c {
+                StreamingConfigurationEnum::NotSpecified(_) => Some(StreamingConfiguration::NotSpecified),
+                StreamingConfigurationEnum::T1Configuration(t1) => match T1StreamingConfiguration::new(t1) {
+                    None => None,
+                    Some(t1) => Some(StreamingConfiguration::T1(t1)),
+                },
+                StreamingConfigurationEnum::T2Configuration(t2) => match T2StreamingConfiguration::new(t2) {
+                    None => None,
+                    Some(t2) => Some(StreamingConfiguration::T2(t2))
+                }
+            }
+        }
+    }
+}
+
+enum T1StreamingConfiguration {
+    EventsTimeout(EventsTimeoutConfiguration),
+    TracesTimeout(TracesTimeoutConfiguration)
+}
+
+impl T1StreamingConfiguration {
+    pub fn new(grpc_config: &GrpcT1StreamingConfiguration) -> Option<Self> {
+        match grpc_config.configuration.as_ref() {
+            None => None,
+            Some(c) => Some(match c {
+                T1ConfigurationEnum::EventsTimeout(et) => T1StreamingConfiguration::EventsTimeout(EventsTimeoutConfiguration::new(&et)),
+                T1ConfigurationEnum::TracesTimeout(tt) => T1StreamingConfiguration::TracesTimeout(TracesTimeoutConfiguration::new(&tt))
+            })
+        }
+    }
+}
+
+struct EventsTimeoutConfiguration { timeout_ms: u64 }
+
+impl EventsTimeoutConfiguration {
+    pub fn new(grpc_events_timeout: &GrpcT1EventsTimeBasedCaching) -> Self {
+        Self {
+            timeout_ms: grpc_events_timeout.events_timeout_ms as u64
+        }
+    }
+}
+
+struct TracesTimeoutConfiguration { pub timeout_ms: u64 }
+
+impl TracesTimeoutConfiguration {
+    pub fn new(grpc_traces_timeout: &GrpcT1TraceTimeBasedCaching) -> Self {
+        Self {
+            timeout_ms: grpc_traces_timeout.traces_timeout_ms as u64
+        }
+    }
+}
+
+enum T2StreamingConfiguration {
+    LossyCount(LossyCountConfiguration)
+}
+
+impl T2StreamingConfiguration {
+    pub fn new(grpc_config: &GrpcT2StreamingConfiguration) -> Option<Self> {
+        match grpc_config.configuration.as_ref() {
+            None => None,
+            Some(c) => Some(match c {
+                T2ConfigurationEnum::LossyCount(lc) => T2StreamingConfiguration::LossyCount(LossyCountConfiguration {
+                    error: lc.error,
+                    support: lc.support
+                })
+            })
+        }
+    }
+}
+
+struct LossyCountConfiguration { error: f64, support: f64 }
