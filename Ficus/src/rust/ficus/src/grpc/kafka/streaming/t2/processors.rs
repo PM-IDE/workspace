5239f074b93584fdde1faa7ea0eadd22289be9a8
aerooneqq <aerooneQ@yandex.ru>
2025-02-01 21:30:51 +0300

    WIP

--- Ficus/src/rust/ficus/src/grpc/kafka/streaming/t2/processors.rs
+++ Ficus/src/rust/ficus/src/grpc/kafka/streaming/t2/processors.rs
@@ -1,5 +1,10 @@
 use std::collections::HashMap;
+use std::hash::Hash;
+use std::ops::{Add, Sub};
+use std::rc::Rc;
+use std::time::Duration;
 use bxes_kafka::consumer::bxes_kafka_consumer::BxesKafkaTrace;
+use chrono::{DateTime, Utc};
 use uuid::Uuid;
 use crate::features::streaming::counters::lossy_count::LossyCount;
 use crate::grpc::kafka::models::XesFromBxesKafkaTraceCreatingError;
@@ -13,5 +18,66 @@ pub struct T2LossyCountStreamingProcessor {
 impl T2LossyCountStreamingProcessor {
     pub fn observe(&mut self, trace: BxesKafkaTrace, context: &mut PipelineContext) -> Result<(), XesFromBxesKafkaTraceCreatingError> {
         Ok(())
+    }
+}
+
+struct SlidingWindowEntry<TValue> {
+    value: TValue,
+    timestamp: DateTime<Utc>
+}
+
+impl<TValue> SlidingWindowEntry<TValue> {
+    pub fn new(value: TValue, timestamp: DateTime<Utc>) -> Self {
+        Self {
+            value,
+            timestamp
+        }
+    }
+}
+
+#[derive(PartialEq)]
+pub enum InvalidationResult {
+    Invalidate,
+    Retain
+}
+
+pub type Invalidator<TValue> = Rc<Box<dyn Fn(&TValue, &DateTime<Utc>) -> InvalidationResult>>;
+
+pub struct SlidingWindowProcessor<TKey: Hash + Eq, TValue> {
+    storage: HashMap<TKey, SlidingWindowEntry<TValue>>,
+    invalidator: Invalidator<TValue>
+}
+
+impl<TKey: Hash + Eq, TValue> SlidingWindowProcessor<TKey, TValue> {
+    pub fn new(invalidator: Invalidator<TValue>) -> Self {
+        Self {
+            storage: HashMap::new(),
+            invalidator
+        }
+    }
+    
+    pub fn new_time(invalidation_duration: Duration) -> Self {
+        Self {
+            storage: HashMap::new(),
+            invalidator: Rc::new(Box::new(move |_, stamp| {
+                match stamp.add(invalidation_duration) < Utc::now() {
+                    true => InvalidationResult::Invalidate,
+                    false => InvalidationResult::Retain
+                }
+            }))
+        }
+    }
+
+    pub fn add_current_stamp(&mut self, key: TKey, value: TValue) {
+        self.add(key, value, Utc::now());
+    }
+    
+    pub fn add(&mut self, key: TKey, value: TValue, stamp: DateTime<Utc>) {
+        self.storage.insert(key, SlidingWindowEntry::new(value, stamp));
+    }
+    
+    pub fn invalidate(&mut self) {
+        let invalidator = self.invalidator.clone();
+        self.storage.retain(|_, value| invalidator(&value.value, &value.timestamp) == InvalidationResult::Retain)
     }
 }
