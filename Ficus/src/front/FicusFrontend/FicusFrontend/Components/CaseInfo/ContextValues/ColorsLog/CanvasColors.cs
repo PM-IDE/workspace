9fd41ed9d632fe50334237195d4ceb83775127f8
aerooneqq <aerooneQ@yandex.ru>
2025-02-16 18:39:11 +0300

    WIP

---
+++ Ficus/src/front/FicusFrontend/FicusFrontend/Components/CaseInfo/ContextValues/ColorsLog/CanvasColors.cs
@@ -0,0 +1,9 @@
+﻿using System.Drawing;
+
+namespace FicusFrontend.Components.CaseInfo.ContextValues.ColorsLog;
+
+public static class CanvasColors
+{
+  public static Color Background { get; } = Color.FromArgb(37, 37, 37);
+  public static Color Axis { get; } = Color.FromArgb(42, 42, 42);
+}
