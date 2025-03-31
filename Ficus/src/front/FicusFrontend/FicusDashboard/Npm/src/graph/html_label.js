563b950cc825866f778b0934361bc17928a09d80
aerooneqq <aerooneq@yandex.ru>
2025-03-31 14:19:44 +0300

    WIP

--- Ficus/src/front/FicusFrontend/FicusDashboard/Npm/src/graph/html_label.js
+++ Ficus/src/front/FicusFrontend/FicusDashboard/Npm/src/graph/html_label.js
@@ -10,22 +10,34 @@ export function createHtmlLabel(node) {
     return null;
   }
 
+  let sortedHistogramEntries = softwareData.histogram.toSorted((f, s) => s.count - f.count);
+  let eventClassesDescription = sortedHistogramEntries.map((entry) => {
+    return `
+        <div style="display: flex; flex-direction: row; height: 20px; align-items: center">
+            <div style="width: 15px; height: 15px; background-color: ${getOrCreateColor(entry.name)}"></div>
+            <div>${entry.name}</div>
+        </div>
+      `;
+  });
+
   let summedCount = Math.max(...softwareData.histogram.map(entry => entry.count));
-  let histogramDivs = softwareData.histogram.toSorted((f, s) => s.count - f.count).map((entry) => {
-      let divWidth = nodeWidthPx * (entry.count / summedCount);
-      return `<div style="width: ${divWidth}px; height: 10px; background-color: ${getOrCreateColor(entry.name)}"></div>`;
-    }
-  );
+  let histogramDivs = sortedHistogramEntries.map((entry) => {
+    let divWidth = (entry.count / summedCount) * 100;
+    return `<div style="width: ${divWidth}%; height: 10px; background-color: ${getOrCreateColor(entry.name)}"></div>`;
+  });
 
   let nodeColor = belongsToRootSequence(node) ? graphColor.rootSequenceColor : graphColor.nodeBackground;
 
   return `
-          <div style="width: ${nodeWidthPx}px; height: ${nodeHeightPx}px; background: ${nodeColor}">
+          <div style="background: ${nodeColor}; min-width: ${nodeWidthPx}px; min-height: ${nodeHeightPx}px">
               <div style="width: 100%; text-align: center; color: ${graphColor.labelColor}">
                   ${node.label}
               </div>
               <div style="width: 100%; display: flex; flex-direction: row;">
                   ${histogramDivs.join('\n')}
+              </div>
+              <div style="width: 100%; display: flex; flex-direction: column;">
+                  ${eventClassesDescription.join('\n')}
               </div>
           </div>
         `;
