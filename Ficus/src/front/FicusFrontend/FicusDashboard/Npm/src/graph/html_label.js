985e650802b1b936a1ab6437930d9a8dec17d8c6
aerooneqq <aerooneq@yandex.ru>
2025-04-07 21:20:07 +0300

    Better traces representation

--- Ficus/src/front/FicusFrontend/FicusDashboard/Npm/src/graph/html_label.js
+++ Ficus/src/front/FicusFrontend/FicusDashboard/Npm/src/graph/html_label.js
@@ -20,12 +20,13 @@ export function createHtmlLabel(node) {
   let sortedHistogramEntries = softwareData.histogram.toSorted((f, s) => s.count - f.count);
   let nodeColor = belongsToRootSequence(node) ? graphColor.rootSequenceColor : graphColor.nodeBackground;
   let timeAnnotationColor = getTimeAnnotationColor(node.relativeExecutionTime);
-  let allTraceIds = findAllRelatedTraceIds(node);
+  let allTraceIds = [...findAllRelatedTraceIds(node).values()];
+  allTraceIds.sort((f, s) => f - s);
 
   return `
           <div style="background: ${nodeColor}; min-width: ${nodeWidthPx}px; min-height: ${nodeHeightPx}px">
               <div style="width: 100%; text-align: center; color: ${graphColor.labelColor}; background-color: ${timeAnnotationColor}">
-                  ${node.label} [${node.executionTime}] ${[...allTraceIds.values().map(x => x.toString())].join(" ")}
+                  ${node.label} [${node.executionTime}] ${createTracesDescription(allTraceIds)}
               </div>
               <div style="width: 100%; display: flex; flex-direction: row;">
                   ${createHistogram(sortedHistogramEntries).join('\n')}
@@ -81,4 +82,31 @@ function createEventClassesDescription(sortedHistogramEntries) {
         </div>
       `;
   });
+}
+
+function createTracesDescription(tracesIds) {
+  let result = "";
+  let index = 0;
+  let groupStartIndex = 0;
+
+  while (index < tracesIds.length) {
+    groupStartIndex = index;
+    while (index < tracesIds.length - 1 && tracesIds[index] + 1 === tracesIds[index + 1]) {
+      index += 1;
+    }
+
+    if (groupStartIndex === index) {
+      result += `${tracesIds[groupStartIndex]}, `;
+    } else {
+      result += `${tracesIds[groupStartIndex]}..${tracesIds[index]}, `;
+    }
+
+    index += 1;
+  }
+  
+  if (result.length < 2) {
+    return "No traces";
+  }
+
+  return result.substring(0, result.length - 2);
 }
