c6ee33dadfa39124fa2b9cb12bdd002643a5e1d2
aerooneqq <aerooneq@yandex.ru>
2025-04-13 18:19:41 +0300

    Add json serialization for extraction config

--- Ficus/src/rust/ficus/src/features/discovery/timeline/software_data/extraction.rs
+++ Ficus/src/rust/ficus/src/features/discovery/timeline/software_data/extraction.rs
@@ -1,47 +1,70 @@
 use std::fmt::Debug;
-use getset::Getters;
+use derive_new::new;
+use getset::{Getters, Setters};
+use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Debug, Getters)]
+#[derive(Clone, Debug, Setters, Getters, Serialize, Deserialize)]
 pub struct SoftwareDataExtractionInfo {
-  #[getset(get="pub")] allocation: ExtractionInfo<AllocationExtractionInfo>,
-  #[getset(get="pub")] exceptions: ExtractionInfo<ExceptionExtractionInfo>,
-  #[getset(get="pub")] http: ExtractionInfo<HTTPExtractionInfo>,
+  #[getset(get="pub", set="pub")] allocation: Option<ExtractionInfo<AllocationExtractionInfo>>,
+  #[getset(get="pub", set="pub")] exceptions: Option<ExtractionInfo<ExceptionExtractionInfo>>,
+  #[getset(get="pub", set="pub")] http: Option<ExtractionInfo<HTTPExtractionInfo>>,
 
-  #[getset(get="pub")] method_inlining_success: ExtractionInfo<()>,
-  #[getset(get="pub")] method_inlining_failed: ExtractionInfo<MethodInliningFailedExtractionInfo>,
+  #[getset(get="pub", set="pub")] method_inlining_success: Option<ExtractionInfo<()>>,
+  #[getset(get="pub", set="pub")] method_inlining_failed: Option<ExtractionInfo<MethodInliningFailedExtractionInfo>>,
 
-  #[getset(get="pub")] contention: ExtractionInfo<ContentionExtractionInfo>,
-  #[getset(get="pub")] socket: ExtractionInfo<SocketExtractionInfo>,
-  #[getset(get="pub")] thread: ExtractionInfo<ThreadExtractionInfo>,
+  #[getset(get="pub", set="pub")] contention: Option<ExtractionInfo<ContentionExtractionInfo>>,
+  #[getset(get="pub", set="pub")] socket: Option<ExtractionInfo<SocketExtractionInfo>>,
+  #[getset(get="pub", set="pub")] thread: Option<ExtractionInfo<ThreadExtractionInfo>>,
 
-  #[getset(get="pub")] array_pool_array_created: ExtractionInfo<ArrayPoolExtractionInfo>,
-  #[getset(get="pub")] array_pool_array_rented: ExtractionInfo<ArrayPoolExtractionInfo>,
-  #[getset(get="pub")] array_pool_array_returned: ExtractionInfo<ArrayPoolExtractionInfo>,
-  #[getset(get="pub")] array_pool_array_trimmed: ExtractionInfo<ArrayPoolExtractionInfo>,
-  
-  #[getset(get="pub")] assembly_load: ExtractionInfo<AssemblyExtractionInfo>,
-  #[getset(get="pub")] assembly_unload: ExtractionInfo<AssemblyExtractionInfo>,
+  #[getset(get="pub", set="pub")] array_pool_array_created: Option<ExtractionInfo<ArrayPoolExtractionInfo>>,
+  #[getset(get="pub", set="pub")] array_pool_array_rented: Option<ExtractionInfo<ArrayPoolExtractionInfo>>,
+  #[getset(get="pub", set="pub")] array_pool_array_returned: Option<ExtractionInfo<ArrayPoolExtractionInfo>>,
+  #[getset(get="pub", set="pub")] array_pool_array_trimmed: Option<ExtractionInfo<ArrayPoolExtractionInfo>>,
+
+  #[getset(get="pub", set="pub")] assembly_load: Option<ExtractionInfo<AssemblyExtractionInfo>>,
+  #[getset(get="pub", set="pub")] assembly_unload: Option<ExtractionInfo<AssemblyExtractionInfo>>,
 }
 
-#[derive(Clone, Debug, Getters)]
+impl SoftwareDataExtractionInfo {
+  pub fn empty() -> Self {
+    Self {
+      allocation: None,
+      exceptions: None,
+      http: None,
+      method_inlining_success: None,
+      method_inlining_failed: None,
+      contention: None,
+      socket: None,
+      thread: None,
+      array_pool_array_created: None,
+      array_pool_array_rented: None,
+      array_pool_array_returned: None,
+      array_pool_array_trimmed: None,
+      assembly_load: None,
+      assembly_unload: None,
+    }
+  }
+}
+
+#[derive(Clone, Debug, Getters, Serialize, Deserialize, new)]
 pub struct ExtractionInfo<TConcreteInfo: Clone + Debug> {
   #[getset(get="pub")] event_class_regex: String,
   #[getset(get="pub")] info: TConcreteInfo,
 }
 
-#[derive(Clone, Debug, Getters)]
+#[derive(Clone, Debug, Getters, Serialize, Deserialize, new)]
 pub struct AllocationExtractionInfo {
   #[getset(get="pub")] type_name_attr: String,
   #[getset(get="pub")] allocated_count_attr: String,
   #[getset(get="pub")] object_size_attr: String
 }
 
-#[derive(Clone, Debug, Getters)]
+#[derive(Clone, Debug, Getters, Serialize, Deserialize, new)]
 pub struct ExceptionExtractionInfo {
   #[getset(get="pub")] type_name_attr: String
 }
 
-#[derive(Clone, Debug, Getters)]
+#[derive(Clone, Debug, Getters, Serialize, Deserialize, new)]
 pub struct HTTPExtractionInfo {
   #[getset(get="pub")] host_attr: String,
   #[getset(get="pub")] port_attr: String,
@@ -50,33 +73,33 @@ pub struct HTTPExtractionInfo {
   #[getset(get="pub")] query_attr: String,
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Serialize, Deserialize, new)]
 pub struct MethodInliningSuccessExtractionInfo {}
 
-#[derive(Clone, Debug, Getters)]
+#[derive(Clone, Debug, Getters, Serialize, Deserialize, new)]
 pub struct MethodInliningFailedExtractionInfo {
   #[getset(get="pub")] reason_attr: String
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Serialize, Deserialize, new)]
 pub struct ContentionExtractionInfo {}
 
-#[derive(Clone, Debug, Getters)]
+#[derive(Clone, Debug, Getters, Serialize, Deserialize, new)]
 pub struct SocketExtractionInfo {
   #[getset(get="pub")] address_attr: String
 }
 
-#[derive(Clone, Debug, Getters)]
+#[derive(Clone, Debug, Getters, Serialize, Deserialize, new)]
 pub struct ThreadExtractionInfo {
   #[getset(get="pub")] thread_id_attr: String
 }
 
-#[derive(Clone, Debug, Getters)]
+#[derive(Clone, Debug, Getters, Serialize, Deserialize, new)]
 pub struct ArrayPoolExtractionInfo {
   #[getset(get="pub")] buffer_id: String
 }
 
-#[derive(Clone, Debug, Getters)]
+#[derive(Clone, Debug, Getters, Serialize, Deserialize, new)]
 pub struct AssemblyExtractionInfo {
   name_attr: String
 }
--- Ficus/src/rust/ficus/src/features/discovery/timeline/software_data/mod.rs
+++ Ficus/src/rust/ficus/src/features/discovery/timeline/software_data/mod.rs
@@ -1,2 +1,2 @@
 pub mod models;
-mod extraction;
+pub mod extraction;
