e18611dbca3bb146f94e20f84e9423631f4af9d0
aerooneqq <aerooneq@yandex.ru>
2025-05-15 12:09:52 +0300

    Fix stupid bug

--- Ficus/src/rust/ficus/src/features/discovery/timeline/software_data/extractors/sockets.rs
+++ Ficus/src/rust/ficus/src/features/discovery/timeline/software_data/extractors/sockets.rs
@@ -24,7 +24,7 @@ impl<'a> SoftwareDataExtractor for SocketsDataExtractor<'a> {
       }),
 
       (self.config.socket_accept_stop().as_ref().map(|c| c.event_class_regex()), &|_| { Ok(Some(SocketEvent::AcceptStop)) }),
-      (self.config.socket_connect_stop().as_ref().map(|c| c.event_class_regex()), &|_| { Ok(Some(SocketEvent::AcceptStop)) }),
+      (self.config.socket_connect_stop().as_ref().map(|c| c.event_class_regex()), &|_| { Ok(Some(SocketEvent::ConnectStop)) }),
 
       (self.config.socket_connect_failed().as_ref().map(|c| c.event_class_regex()), &|event| {
         create_connect_accept_failed(event, self.config.socket_connect_failed().as_ref().unwrap().info(), true)
