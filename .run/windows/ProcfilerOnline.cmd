8a04f7f4e51badd80df0b68ddc9a26661503fda3
aerooneqq <aerooneQ@yandex.ru>
2025-06-22 18:36:39 +0300

    Rebuild web app project too

--- .run/windows/ProcfilerOnline.cmd
+++ .run/windows/ProcfilerOnline.cmd
@@ -6,6 +6,7 @@ set OnlineProcfilerSettings__KafkaSettings__BootstrapServers=localhost:9092
 
 dotnet build %PM_IDE_ROOT%/All.sln -c Release
 dotnet build %PM_IDE_ROOT%/Procfiler/src/dotnet/ProcfilerLoggerProvider/ -c Release
+dotnet build %PM_IDE_ROOT%/Procfiler/test_data/source/WFWebApp -c Release
 
 %PM_IDE_ROOT%/Procfiler/src/dotnet/ProcfilerOnline/bin/Release/net9.0/ProcfilerOnline.exe procfiler-online collect-online^
  -dll-path %PM_IDE_ROOT%\Procfiler\test_data\source\WFWebApp\bin\Release\net9.0\WFWebApp.dll^
