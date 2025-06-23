820a8ac98813db9291ccfba301bbb63287825230
aerooneqq <aerooneQ@yandex.ru>
2025-06-23 23:20:49 +0300

    Starting working on Aspire integration

---
+++ Procfiler/src/ProcfilerOnline.Aspire/DistributedApplicationBuilderExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using Aspire.Hosting;
+using Aspire.Hosting.ApplicationModel;
+
+namespace ProcfilerOnline.Aspire;
+
+public static class DistributedApplicationBuilderExtensions
+{
+  public static IResourceBuilder<ExecutableResource> AddLocalProcfilerExecutable<TProject>(
+    this IDistributedApplicationBuilder builder,
+    string name,
+    string localProcfilerExecutablePath
+  ) where TProject : IProjectMetadata, new()
+  {
+    var projectPath = new TProject().ProjectPath;
+    var projectDir = Path.GetDirectoryName(projectPath)!;
+
+    return builder.AddExecutable(
+      $"procfiler-{name}",
+      localProcfilerExecutablePath,
+      projectDir,
+      "collect-online",
+      "-command",
+      $"""
+      "dotnet run {projectDir}"
+      """
+    );
+  }
+}
