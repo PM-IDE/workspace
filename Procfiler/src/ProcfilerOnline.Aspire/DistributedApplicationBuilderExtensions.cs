88eb28a164f1cdfdefc59220c9449c9409f3d926
aerooneqq <aerooneq@yandex.ru>
2025-06-24 17:19:14 +0300

    Add env vars for ProcfilerOnline, fix bug with project name

--- Procfiler/src/ProcfilerOnline.Aspire/DistributedApplicationBuilderExtensions.cs
+++ Procfiler/src/ProcfilerOnline.Aspire/DistributedApplicationBuilderExtensions.cs
@@ -12,21 +12,28 @@ public static class DistributedApplicationBuilderExtensions
   ) where TProject : IProjectMetadata, new()
   {
     var projectPath = new TProject().ProjectPath;
-    var projectName = Path.GetFileName(projectPath);
+    var projectName = Path.GetFileNameWithoutExtension(projectPath);
 
     var projectResource = builder.AddProject<TProject>(name);
-    var executableResource = builder.AddExecutable(
-      $"procfiler-{name}",
-      localProcfilerExecutablePath,
-      Path.GetDirectoryName(projectPath)!,
-      "collect-online",
-      "-csproj",
-      projectPath,
-      "--target-methods-regex",
-      projectName,
-      "--methods-filter-regex",
-      projectName
-    );
+
+    var executableResource = builder
+      .AddExecutable(
+        $"procfiler-{name}",
+        localProcfilerExecutablePath,
+        Path.GetDirectoryName(projectPath)!,
+        "collect-online",
+        "-csproj",
+        projectPath,
+        "--target-methods-regex",
+        projectName,
+        "--methods-filter-regex",
+        projectName
+      )
+      .WithEnvironment("ProduceEventsToKafka", "true")
+      .WithEnvironment("ProduceBxesKafkaEvents", "true")
+      .WithEnvironment("ProduceGcEvents", "false")
+      .WithEnvironment("OnlineProcfilerSettings__KafkaSettings__TopicName", "my-topic")
+      .WithEnvironment("OnlineProcfilerSettings__KafkaSettings__BootstrapServers", "localhost:9092");
 
     foreach (var resourceAnnotation in projectResource.Resource.Annotations)
     {
