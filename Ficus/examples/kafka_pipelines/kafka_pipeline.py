3ce3e2ebe22668c94eaf1d076dc2bcf6292eb10a
aerooneqq <aerooneq@yandex.ru>
2025-07-01 18:28:45 +0300

    Update example

--- Ficus/examples/kafka_pipelines/kafka_pipeline.py
+++ Ficus/examples/kafka_pipelines/kafka_pipeline.py
@@ -5,20 +5,19 @@ execute_pipeline(
     'MySubscription',
     'Pipeline',
     [
-        PrintEventLogInfo(),
-        TracesDiversityDiagramCanvas(),
         RemainOnlyMethodStartEvents(),
         SetMethodsDisplayName(),
         ShortenAllocationType(),
         PrepareSoftwareLog(time_attribute = 'QpcStamp'),
         TerminateIfEmptyLog(),
+        TracesDiversityDiagramCanvas(),
         AddStartEndArtificialEvents(),
         TracesDiversityDiagramCanvas(),
         DiscoverLoopsStrict(),
         CreateLogFromActivitiesInstances(strategy=UndefinedActivityHandlingStrategy.InsertAllEvents),
         TracesDiversityDiagramCanvas(),
         DiscoverRootSequenceGraph(root_sequence_kind=RootSequenceKind.FindBest,
-                                  merge_sequences_of_events=True),
+                                  merge_sequences_of_events=False),
         AnnotateGraphWithTime(TimeAnnotationKind.Mean),
     ]
 )
