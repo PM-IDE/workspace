5e23a8a164f0abb62b5dc8e796b3a5a55b03e8b6
aerooneqq <aerooneq@yandex.ru>
2025-07-02 18:35:07 +0300

    Add OCEL logging

---
+++ Procfiler/src/dotnet/ProcfilerEventSources/OcelEventsSource.cs
@@ -0,0 +1,30 @@
+using System.Diagnostics.Tracing;
+
+namespace ProcfilerEventSources;
+
+[EventSource(Name = $"{nameof(OcelEventsSource)}")]
+public class OcelEventsSource : EventSource
+{
+  private const int OcelEventId = 7000;
+  private const int OcelActivityStartId = 7001;
+  private const int OcelActivityEndId = 7002;
+
+
+  public static OcelEventsSource Instance { get; } = new();
+
+
+  private OcelEventsSource()
+  {
+  }
+
+
+  [Event(OcelEventId, Level = EventLevel.LogAlways)]
+  public void OcelEvent(int objectId, string? objectCategory, string attributes) =>
+    WriteEvent(OcelEventId, objectId, objectCategory, attributes);
+
+  [Event(OcelActivityStartId, Level = EventLevel.LogAlways)]
+  public void OcelActivityStart(Guid activityId, string activity) => WriteEvent(OcelActivityStartId, activityId, activity);
+
+  [Event(OcelActivityEndId, Level = EventLevel.LogAlways)]
+  public void OcelActivityEnd(Guid activityId, string activity) => WriteEvent(OcelActivityEndId, activityId, activity);
+}
---
+++ Procfiler/src/dotnet/ProcfilerLoggerProvider/OcelLogger.cs
@@ -0,0 +1,28 @@
+using System.Runtime.CompilerServices;
+using ProcfilerEventSources;
+
+namespace ProcfilerLoggerProvider;
+
+public static class OcelLogger
+{
+  public readonly struct OcelActivityCookie(string name, Guid activityId) : IDisposable
+  {
+    public void Dispose()
+    {
+      OcelEventsSource.Instance.OcelActivityEnd(activityId, name);
+    }
+  }
+
+
+  public static void LogObject<T>(T obj, string? category = null) where T : class
+  {
+    OcelEventsSource.Instance.OcelEvent(RuntimeHelpers.GetHashCode(obj), category, string.Empty);
+  }
+
+  public static OcelActivityCookie StartOcelActivity(string name)
+  {
+    var activityId = Guid.NewGuid();
+    OcelEventsSource.Instance.OcelActivityStart(activityId, name);
+    return new OcelActivityCookie(name, activityId);
+  }
+}
