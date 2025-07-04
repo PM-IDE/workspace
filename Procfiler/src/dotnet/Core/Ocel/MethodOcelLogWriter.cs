dcc0517ed0953c4e4f2b5917dbaa02e5cec9a3fd
aerooneqq <aerooneq@yandex.ru>
2025-07-04 21:46:17 +0300

    Small refactorings

---
+++ Procfiler/src/dotnet/Core/Ocel/MethodOcelLogWriter.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+using Core.Events.EventRecord;
+using Core.Utils;
+using Microsoft.Extensions.Logging;
+
+namespace Core.Ocel;
+
+public class MethodOcelLogWriter(string outputFileName, IProcfilerLogger logger)
+{
+  public void Process(List<EventRecordWithMetadata> trace)
+  {
+    var stack = new List<(Guid Id, string Name, DateTimeOffset StartTime, Dictionary<string, List<int>> Events)>();
+
+    using var fs = File.OpenWrite(outputFileName);
+    using var sw = new StreamWriter(fs);
+
+    var categories = new HashSet<string>();
+    foreach (var evt in trace)
+    {
+      if (evt.IsOcelObjectEvent(out var _, out var category))
+      {
+        categories.Add(category ?? string.Empty);
+      }
+    }
+
+    var orderedCategories = categories.Order().ToList();
+    sw.WriteLine("event_activity;start;end;" + string.Join(';', orderedCategories));
+
+    foreach (var evt in trace)
+    {
+      if (evt.IsOcelActivityBegin(out var activityId, out var activityName))
+      {
+        stack.Add((activityId, activityName, evt.Time.LoggedAt.ToUniversalTime(), []));
+      }
+      else if (evt.IsOcelActivityEnd(out activityId, out activityName))
+      {
+        if (stack.FindIndex(e => e.Id == activityId) is var entryIndex and >= 0)
+        {
+          var entry = stack[entryIndex];
+          var sb = new StringBuilder($"{entry.Name};{entry.StartTime:O};{evt.Time.LoggedAt.ToUniversalTime():O};");
+          foreach (var category in orderedCategories)
+          {
+            sb.Append($"[{string.Join(',', entry.Events.GetValueOrDefault(category, []))}];");
+          }
+
+          sb.Remove(sb.Length - 1, 1);
+          sw.WriteLine(sb);
+
+          stack.RemoveAt(entryIndex);
+        }
+        else
+        {
+          logger.LogWarning("Failed to find activity with ID {Id}", activityId);
+        }
+      }
+      else if (evt.IsOcelObjectEvent(out var objectId, out var category))
+      {
+        foreach (var entry in stack)
+        {
+          entry.Events.GetOrCreate(category ?? string.Empty, static () => []).Add(objectId);
+        }
+      }
+    }
+  }
+}
--- Procfiler/src/dotnet/Procfiler/src/Commands/CollectClrEvents/Split/SplitEventsByMethodCommand.cs
+++ Procfiler/src/dotnet/Procfiler/src/Commands/CollectClrEvents/Split/SplitEventsByMethodCommand.cs
@@ -1,6 +1,7 @@
 using Core.CommandLine;
 using Core.Container;
 using Core.Events.EventRecord;
+using Core.Ocel;
 using Core.Utils;
 using Procfiler.Commands.CollectClrEvents.Base;
 using Procfiler.Commands.CollectClrEvents.Context;
@@ -13,6 +14,7 @@ using Procfiler.Core.Serialization.Core;
 using Procfiler.Core.Serialization.Xes;
 using Procfiler.Core.SplitByMethod;
 using Procfiler.Utils;
+using Serilog;
 
 namespace Procfiler.Commands.CollectClrEvents.Split;
 
@@ -116,58 +118,9 @@ public class SplitEventsByMethodCommand(
     foreach (var (index, trace) in methodTraces.Index())
     {
       var outputFileName = Path.Combine(ocelOutputDir, $"{index}_{name}.csv");
-      var stack = new List<(Guid Id, string Name, DateTimeOffset StartTime, Dictionary<string, List<int>> Events)>();
+      var writer = new MethodOcelLogWriter(outputFileName, Logger);
 
-      using var fs = File.OpenWrite(outputFileName);
-      using var sw = new StreamWriter(fs);
-
-      var categories = new HashSet<string>();
-      foreach (var evt in trace)
-      {
-        if (evt.IsOcelObjectEvent(out var _, out var category))
-        {
-          categories.Add(category ?? string.Empty);
-        }
-      }
-
-      var orderedCategories = categories.Order().ToList();
-      sw.WriteLine("event_activity;start;end;" + string.Join(';', orderedCategories));
-
-      foreach (var evt in trace)
-      {
-        if (evt.IsOcelActivityBegin(out var activityId, out var activityName))
-        {
-          stack.Add((activityId, activityName, evt.Time.LoggedAt.ToUniversalTime(), []));
-        }
-        else if (evt.IsOcelActivityEnd(out activityId, out activityName))
-        {
-          if (stack.FindIndex(e => e.Id == activityId) is var entryIndex and >= 0)
-          {
-            var entry = stack[entryIndex];
-            var sb = new StringBuilder($"{entry.Name};{entry.StartTime:O};{evt.Time.LoggedAt.ToUniversalTime():O};");
-            foreach (var category in orderedCategories)
-            {
-              sb.Append($"[{string.Join(',', entry.Events.GetValueOrDefault(category, []))}];");
-            }
-
-            sb.Remove(sb.Length - 1, 1);
-            sw.WriteLine(sb);
-
-            stack.RemoveAt(entryIndex);
-          }
-          else
-          {
-            Logger.LogWarning("Failed to find activity with ID {Id}", activityId);
-          }
-        }
-        else if (evt.IsOcelObjectEvent(out var objectId, out var category))
-        {
-          foreach (var entry in stack)
-          {
-            entry.Events.GetOrCreate(category ?? string.Empty, static () => []).Add(objectId);
-          }
-        }
-      }
+      writer.Process(trace);
     }
   }
 
