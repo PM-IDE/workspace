9c4ca00110cdbdd31088ed951c5c1fbaf1fcacf2
aerooneqq <aerooneq@yandex.ru>
2025-07-05 21:23:33 +0300

    Add more API for logging

--- Procfiler/src/dotnet/ProcfilerEventSources/OcelEventsSource.cs
+++ Procfiler/src/dotnet/ProcfilerEventSources/OcelEventsSource.cs
@@ -6,8 +6,11 @@ namespace ProcfilerEventSources;
 public class OcelEventsSource : EventSource
 {
   private const int OcelEventId = 7000;
-  private const int OcelActivityStartId = 7001;
+  private const int OcelActivityBeginId = 7001;
   private const int OcelActivityEndId = 7002;
+  private const int OcelActivitiesStartId = 7003;
+  private const int OcelActivitiesEndId = 7004;
+  private const int OcelAttachedToActivityEventId = 7005;
 
 
   public static OcelEventsSource Instance { get; } = new();
@@ -22,9 +25,19 @@ public class OcelEventsSource : EventSource
   public void OcelEvent(int objectId, string? objectCategory, string attributes) =>
     WriteEvent(OcelEventId, objectId, objectCategory, attributes);
 
-  [Event(OcelActivityStartId, Level = EventLevel.LogAlways)]
-  public void OcelActivityBegin(Guid activityId, string activity) => WriteEvent(OcelActivityStartId, activityId, activity);
+  [Event(OcelActivityBeginId, Level = EventLevel.LogAlways)]
+  public void OcelActivityBegin(Guid activityId, string activity) => WriteEvent(OcelActivityBeginId, activityId, activity);
 
   [Event(OcelActivityEndId, Level = EventLevel.LogAlways)]
   public void OcelActivityEnd(Guid activityId, string activity) => WriteEvent(OcelActivityEndId, activityId, activity);
+
+  [Event(OcelActivitiesStartId, Level = EventLevel.LogAlways)]
+  public void OcelActivitiesBegin(Guid activitiesBatchId, string names) => WriteEvent(OcelActivityEndId, activitiesBatchId, names);
+
+  [Event(OcelActivitiesEndId, Level = EventLevel.LogAlways)]
+  public void OcelActivitiesEnd(Guid activitiesBatchId, string names) => WriteEvent(OcelActivityEndId, activitiesBatchId, names);
+
+  [Event(OcelAttachedToActivityEventId, Level = EventLevel.LogAlways)]
+  public void OcelAttachedToActivityEvent(int objectId, string activityName, string? objectCategory, string attributes) =>
+    WriteEvent(OcelEventId, objectId, activityName, objectCategory, attributes);
 }
--- Procfiler/src/dotnet/ProcfilerLoggerProvider/OcelLogger.cs
+++ Procfiler/src/dotnet/ProcfilerLoggerProvider/OcelLogger.cs
@@ -13,10 +13,23 @@ public static class OcelLogger
     }
   }
 
+  public readonly struct OcelActivitiesCookie(Guid batchId, string joinedNames) : IDisposable
+  {
+    public void Dispose()
+    {
+      OcelEventsSource.Instance.OcelActivitiesEnd(batchId, joinedNames);
+    }
+  }
+
 
   public static void LogObject<T>(T obj, string? category = null) where T : class
   {
     OcelEventsSource.Instance.OcelEvent(RuntimeHelpers.GetHashCode(obj), category, string.Empty);
+  }
+
+  public static void LogAttachedObject<T>(T obj, string activityName, string? category = null) where T : class
+  {
+    OcelEventsSource.Instance.OcelAttachedToActivityEvent(RuntimeHelpers.GetHashCode(obj), activityName, category, string.Empty);
   }
 
   public static OcelActivityCookie StartOcelActivity(string name)
@@ -24,5 +37,15 @@ public static class OcelLogger
     var activityId = Guid.NewGuid();
     OcelEventsSource.Instance.OcelActivityBegin(activityId, name);
     return new OcelActivityCookie(name, activityId);
+  }
+
+  public static OcelActivitiesCookie StartOcelActivities(string[] names)
+  {
+    var batchId = Guid.NewGuid();
+    var joinedNames = string.Join(';', names);
+
+    OcelEventsSource.Instance.OcelActivitiesBegin(batchId, joinedNames);
+
+    return new OcelActivitiesCookie(batchId, joinedNames);
   }
 }
