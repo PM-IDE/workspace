46df3595bf410d8ded750e735cc9d7ffa84d7378
aerooneqq <aerooneq@yandex.ru>
2025-08-19 14:57:07 +0300

    Refactorings

---
+++ Ficus/src/go/balancer/backends/materialization.go
@@ -0,0 +1,79 @@
+package backends
+
+import (
+	grpc_models "balancer/models"
+	"fmt"
+	"sync"
+
+	cmap "github.com/orcaman/concurrent-map/v2"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+type PipelinePartDescriptor struct {
+	name string
+}
+
+func NewPipelinePartDescriptor(name string) PipelinePartDescriptor {
+	return PipelinePartDescriptor{name}
+}
+
+type BackendDescriptor struct {
+	name          string
+	pipelineParts []PipelinePartDescriptor
+}
+
+func NewBackendDescriptor(name string, pipelineParts []PipelinePartDescriptor) BackendDescriptor {
+	return BackendDescriptor{name, pipelineParts}
+}
+
+func materializeBackendsPipelinePartsDescriptors(urls []string) map[string]BackendDescriptor {
+	descriptors := cmap.New[BackendDescriptor]()
+
+	wg := sync.WaitGroup{}
+
+	for _, url := range urls {
+		wg.Go(func() {
+			desc, err := materializeBackendPipelinePartsDescriptors(url)
+			if err != nil {
+				fmt.Printf("Failed to get descriptor for backend %s %s\n", url, err.Error())
+				return
+			}
+
+			descriptors.Set(url, desc)
+		})
+	}
+
+	wg.Wait()
+
+	return descriptors.Items()
+}
+
+func materializeBackendPipelinePartsDescriptors(url string) (BackendDescriptor, error) {
+	conn, err := grpc.NewClient(url, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return BackendDescriptor{}, err
+	}
+
+	client := grpc_models.NewGrpcBackendServiceClient(conn)
+	info, err := client.GetBackendInfo(context.Background(), &emptypb.Empty{})
+	if err != nil {
+		return BackendDescriptor{}, err
+	}
+
+	pipelineParts := make([]PipelinePartDescriptor, 0, len(info.GetPipelineParts()))
+
+	for _, descriptor := range info.GetPipelineParts() {
+		pipelineParts = append(pipelineParts, NewPipelinePartDescriptor(descriptor.GetName()))
+	}
+
+	desc := NewBackendDescriptor(info.GetName(), pipelineParts)
+
+	if err = conn.Close(); err != nil {
+		return BackendDescriptor{}, err
+	}
+
+	return desc, nil
+}
