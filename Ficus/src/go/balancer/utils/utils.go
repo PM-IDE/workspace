e237cd4d2ad2abdc87e6a554b455e91253d8d056
aerooneqq <aerooneq@yandex.ru>
2025-08-21 23:14:00 +0300

    WIP

--- Ficus/src/go/balancer/utils/utils.go
+++ Ficus/src/go/balancer/utils/utils.go
@@ -12,15 +12,42 @@ func ExecuteWithBackendClient[T any](
 	url string,
 	action func(grpcmodels.GrpcBackendServiceClient) result.Result[T],
 ) result.Result[T] {
-	clientRes := CreateNewBackendServiceClient(url)
+	return ExecuteWithGrpcClient[grpcmodels.GrpcBackendServiceClient, T](
+		url,
+		func(conn *grpc.ClientConn) grpcmodels.GrpcBackendServiceClient {
+			return grpcmodels.NewGrpcBackendServiceClient(conn)
+		},
+		action,
+	)
+}
+
+func ExecuteWithContextValuesClient[T any](
+	url string,
+	action func(client grpcmodels.GrpcContextValuesServiceClient) result.Result[T],
+) result.Result[T] {
+	return ExecuteWithGrpcClient[grpcmodels.GrpcContextValuesServiceClient, T](
+		url,
+		func(conn *grpc.ClientConn) grpcmodels.GrpcContextValuesServiceClient {
+			return grpcmodels.NewGrpcContextValuesServiceClient(conn)
+		},
+		action,
+	)
+}
+
+func ExecuteWithGrpcClient[TClient any, TReturn any](
+	url string,
+	factory func(*grpc.ClientConn) TClient,
+	action func(TClient) result.Result[TReturn],
+) result.Result[TReturn] {
+	clientRes := CreateNewGrpcServiceClient(url, factory)
 	if clientRes.IsErr() {
-		return result.Err[T](clientRes.Err())
+		return result.Err[TReturn](clientRes.Err())
 	}
 
 	valueResult := action(clientRes.Ok().client)
 
 	if err := clientRes.Ok().connection.Close(); err != nil {
-		return result.Err[T](err)
+		return result.Err[TReturn](err)
 	}
 
 	return valueResult
@@ -31,12 +58,15 @@ type clientWithConnection[TClient any] struct {
 	connection *grpc.ClientConn
 }
 
-func CreateNewBackendServiceClient(url string) result.Result[clientWithConnection[grpcmodels.GrpcBackendServiceClient]] {
+func CreateNewGrpcServiceClient[TClient any](
+	url string,
+	factory func(*grpc.ClientConn) TClient,
+) result.Result[clientWithConnection[TClient]] {
 	conn, err := grpc.NewClient(url, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		return result.Err[clientWithConnection[grpcmodels.GrpcBackendServiceClient]](err)
+		return result.Err[clientWithConnection[TClient]](err)
 	}
 
-	client := grpcmodels.NewGrpcBackendServiceClient(conn)
-	return result.Ok(&clientWithConnection[grpcmodels.GrpcBackendServiceClient]{client, conn})
+	client := factory(conn)
+	return result.Ok(&clientWithConnection[TClient]{client, conn})
 }
