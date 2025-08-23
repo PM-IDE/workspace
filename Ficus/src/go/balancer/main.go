283ad329d947c4d1b467c71753ef05e2b6b7d219
aerooneqq <aerooneq@yandex.ru>
2025-08-23 16:35:47 +0300

    Remove not needed code

--- Ficus/src/go/balancer/main.go
+++ Ficus/src/go/balancer/main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"balancer/grpcmodels"
+	"fmt"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
 func main() {
-	StartServer()
+	lis, err := net.Listen("tcp", ":8081")
+
+	if err != nil {
+		fmt.Printf("failed to listen: %v\n", err)
+		return
+	}
+
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+
+	container := BuildContainer()
+	defer func() {
+		err := container.Logger.Sync()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	}()
+
+	grpcmodels.RegisterGrpcBackendServiceServer(grpcServer, container.BackendService)
+	grpcmodels.RegisterGrpcContextValuesServiceServer(grpcServer, container.ContextValuesService)
+
+	err = grpcServer.Serve(lis)
+	if err != nil {
+		fmt.Printf("error  %v\n", err)
+	}
 }
