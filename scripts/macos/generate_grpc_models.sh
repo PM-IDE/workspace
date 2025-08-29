71279c0896548ce4cd6312f805231cea14f34bf4
aerooneqq <aerooneq@yandex.ru>
2025-08-29 21:34:04 +0300

    Fix grpc models generation script

--- scripts/macos/generate_grpc_models.sh
+++ scripts/macos/generate_grpc_models.sh
@@ -3,9 +3,9 @@ cd ../../Ficus/src/python/infra
 
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
-cd ../../go/balancer/grpcmodels
-protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative --proto_path=../../../../protos/  ../../../../protos/*.proto
+cd ../../go/grpcmodels
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative --proto_path=../../../protos/  ../../../protos/*.proto
 
-cd ../../../front/FicusFrontend/FicusDashboard/Npm
+cd ../../front/FicusFrontend/FicusDashboard/Npm
 sudo node ./node_modules/.bin/proto-loader-gen-types --longs=Number --enums=number --defaults --oneofs --grpcLib=@grpc/grpc-js --outDir=./src/protos/ ../../../../../protos/*.proto --inputTemplate "%s_DONTUSE" --outputTemplate "%s"
 
