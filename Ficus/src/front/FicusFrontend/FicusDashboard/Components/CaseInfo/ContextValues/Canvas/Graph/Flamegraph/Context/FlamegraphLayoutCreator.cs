8ff6dfeb2b43103018eb21f54a3e314760b18e6a
aerooneqq <aerooneq@yandex.ru>
2025-09-23 16:20:58 +0300

    Refactorings

--- Ficus/src/front/FicusFrontend/FicusDashboard/Components/CaseInfo/ContextValues/Canvas/Graph/Flamegraph/Context/FlamegraphLayoutCreator.cs
+++ Ficus/src/front/FicusFrontend/FicusDashboard/Components/CaseInfo/ContextValues/Canvas/Graph/Flamegraph/Context/FlamegraphLayoutCreator.cs
@@ -22,64 +22,79 @@ internal static class FlamegraphLayoutCreator
 
     while (node != endNode)
     {
-      if (data.NodePairs.TryGetValue(node, out var pairNode))
+      if (data.NodePairs.TryGetValue(node, out var nodePair))
       {
         hBlock.InnerBlocks.Add(new SequentialBasicBlock
         {
           NodesSequence = [node]
         });
 
-        var block = new VerticalCompositeBlock();
-
-        var outgoingNodes = data.Edges[node];
-        foreach (var (index, outgoingNode) in outgoingNodes.Index())
-        {
-          if (outgoingNode == pairNode.PairedNode)
-          {
-            block.InnerBlocks.Add(new EdgeBlock());
-          }
-          else
-          {
-            var outgoingNodeEndNode = data.NodePairs.TryGetValue(outgoingNode, out var outgoingNodePair) switch
-            {
-              true => outgoingNodePair,
-              false => pairNode
-            };
-
-            block.InnerBlocks.Add(CreateBlockLayoutEndNodeNotInclusive(data, outgoingNode, outgoingNodeEndNode.PairedNode));
-          }
-
-          if (index < outgoingNodes.Count - 1)
-          {
-            block.InnerBlocks.Add(new SeparatorBlock());
-          }
-        }
+        var block = CreateVerticalBlock(data, node, nodePair);
 
         hBlock.InnerBlocks.Add(block);
-        node = pairNode.PairedNode;
+        node = nodePair.PairedNode;
       }
       else
       {
-        var currentNode = node;
-        var nodesSequence = new List<ulong>();
-
-        while (currentNode != endNode &&
-               !data.NodePairs.ContainsKey(currentNode) &&
-               data.Edges.ContainsKey(currentNode))
-        {
-          nodesSequence.Add(currentNode);
-          currentNode = data.Edges[currentNode].First();
-        }
-
-        hBlock.InnerBlocks.Add(new SequentialBasicBlock
-        {
-          NodesSequence = nodesSequence
-        });
-
-        node = currentNode;
+        var (block, blockEndNode) = CreateSequentialBlock(data, node, endNode);
+        hBlock.InnerBlocks.Add(block);
+        node = blockEndNode;
       }
     }
 
     return hBlock;
+  }
+
+  private static VerticalCompositeBlock CreateVerticalBlock(FlamegraphContextData data, ulong node, NodePair nodePair)
+  {
+    var block = new VerticalCompositeBlock();
+
+    var outgoingNodes = data.Edges[node];
+    foreach (var (index, outgoingNode) in outgoingNodes.Index())
+    {
+      if (outgoingNode == nodePair.PairedNode)
+      {
+        block.InnerBlocks.Add(new EdgeBlock());
+      }
+      else
+      {
+        var outgoingNodeEndNode = data.NodePairs.TryGetValue(outgoingNode, out var outgoingNodePair) switch
+        {
+          true => outgoingNodePair,
+          false => nodePair
+        };
+
+        block.InnerBlocks.Add(CreateBlockLayoutEndNodeNotInclusive(data, outgoingNode, outgoingNodeEndNode.PairedNode));
+      }
+
+      if (index < outgoingNodes.Count - 1)
+      {
+        block.InnerBlocks.Add(new SeparatorBlock());
+      }
+    }
+
+    return block;
+  }
+
+  private static (SequentialBasicBlock Block, ulong NextNode) CreateSequentialBlock(
+    FlamegraphContextData data, ulong node, ulong endNode)
+  {
+    var currentNode = node;
+    var nodesSequence = new List<ulong>();
+
+    while (currentNode != endNode &&
+           !data.NodePairs.ContainsKey(currentNode) &&
+           data.Edges.ContainsKey(currentNode))
+    {
+      nodesSequence.Add(currentNode);
+      currentNode = data.Edges[currentNode].First();
+    }
+
+    var block = new SequentialBasicBlock
+    {
+      NodesSequence = nodesSequence
+    };
+
+    return (block, currentNode);
   }
 }
