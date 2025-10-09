6362735f53dbdb37dc3e6029af4b467fd35cf9f7
aerooneqq <aerooneq@yandex.ru>
2025-10-09 12:55:42 +0300

    Commit forgotten models

---
+++ Ficus/src/front/FicusFrontend/FicusDashboard/Npm/src/protos/ficus/GrpcModelElementOcelAnnotation.ts
@@ -0,0 +1,16 @@
+// Original file: ../../../../../protos/pipelines_and_context.proto
+
+import type { GrpcOcelState_DONTUSE as _ficus_GrpcOcelState_DONTUSE, GrpcOcelState as _ficus_GrpcOcelState } from '../ficus/GrpcOcelState';
+import type { Long } from '@grpc/proto-loader';
+
+export interface GrpcModelElementOcelAnnotation_DONTUSE {
+  'elementId'?: (number | string | Long);
+  'initialState'?: (_ficus_GrpcOcelState_DONTUSE | null);
+  'finalState'?: (_ficus_GrpcOcelState_DONTUSE | null);
+}
+
+export interface GrpcModelElementOcelAnnotation {
+  'elementId': (number);
+  'initialState': (_ficus_GrpcOcelState | null);
+  'finalState': (_ficus_GrpcOcelState | null);
+}
---
+++ Ficus/src/front/FicusFrontend/FicusDashboard/Npm/src/protos/ficus/GrpcOcelState.ts
@@ -0,0 +1,11 @@
+// Original file: ../../../../../protos/pipelines_and_context.proto
+
+import type { GrpcOcelObjectTypeState_DONTUSE as _ficus_GrpcOcelObjectTypeState_DONTUSE, GrpcOcelObjectTypeState as _ficus_GrpcOcelObjectTypeState } from '../ficus/GrpcOcelObjectTypeState';
+
+export interface GrpcOcelState_DONTUSE {
+  'typeStates'?: (_ficus_GrpcOcelObjectTypeState_DONTUSE)[];
+}
+
+export interface GrpcOcelState {
+  'typeStates': (_ficus_GrpcOcelObjectTypeState)[];
+}
