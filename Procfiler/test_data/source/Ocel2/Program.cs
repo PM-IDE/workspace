86d4d38b12053f6ad2de262e27ee603e0b921dbf
aerooneqq <aerooneq@yandex.ru>
2025-10-11 22:41:34 +0300

    Add test project for new OCEL logging methods

---
+++ Procfiler/test_data/source/Ocel2/Program.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.CompilerServices;
+using ProcfilerLoggerProvider;
+
+namespace Ocel2;
+
+public static class Program
+{
+  public static void Main()
+  {
+  }
+
+  private static int ourNextId;
+
+  private static int NextId()
+  {
+    ourNextId++;
+    return ourNextId;
+  }
+
+  private static void Method()
+  {
+    const string Type1 = nameof(Type1);
+    const string Type2 = nameof(Type2);
+    const string Type3 = nameof(Type3);
+
+    var id1 = NextId();
+    var id2 = NextId();
+    var id3 = NextId();
+    OcelLogger.LogObjectAllocated(id1, Type1);
+    OcelLogger.LogObjectAllocated(id2, Type1);
+    OcelLogger.LogObjectAllocated(id3, Type1);
+
+    var id4 = NextId();
+    var id5 = NextId();
+    var id6 = NextId();
+
+    OcelLogger.LogObjectAllocated(id4, Type2);
+    OcelLogger.LogObjectAllocated(id5, Type2);
+    OcelLogger.LogObjectAllocated(id6, Type2);
+
+    Method1();
+
+    var id7 = NextId();
+    OcelLogger.LogMergedObjectAllocated(id7, Type3, id1, id3, id5);
+
+    var id8 = NextId();
+    OcelLogger.LogMergedObjectAllocated(id8, Type1, id2, id4, id6);
+
+    Method1();
+
+    OcelLogger.LogObjectConsumedWithProduce(id7, Type3);
+    OcelLogger.LogObjectConsumedWithProduce(id8, Type2);
+
+    Method1();
+  }
+
+  [MethodImpl(MethodImplOptions.NoInlining)]
+  private static void Method1()
+  {
+  }
+}
