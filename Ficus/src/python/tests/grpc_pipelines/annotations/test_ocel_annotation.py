84f60b4dbe42c521f755adc3f82e6e3f9624a5a0
aerooneqq <aerooneq@yandex.ru>
2025-11-01 21:13:40 +0300

    Add test pipeline part for assertion of OCEL annotation

--- Ficus/src/python/tests/grpc_pipelines/annotations/test_ocel_annotation.py
+++ Ficus/src/python/tests/grpc_pipelines/annotations/test_ocel_annotation.py
@@ -3,16 +3,30 @@ from ....ficus import *
 from ...grpc_pipelines.test_grpc_pipelines import _execute_pipeline, assert_success_pipeline_final_result
 from ...test_data_provider import *
 
+class AssertCorrectOcelAnnotation(PipelinePartWithCallback):
+  def to_grpc_part(self) -> GrpcPipelinePartBase:
+    part = create_simple_get_context_value_part(
+      self.uuid,
+      self.__class__.__name__,
+      const_ocel_annotation,
+    )
+
+    return GrpcPipelinePartBase(simpleContextRequestPart=part)
+
+  def execute_callback(self, values: dict[str, GrpcContextValue]):
+    print(values)
+
 
 def test_ocel_annotation_1():
   _execute_ocel_annotation_test('ocel.bxes', Pipeline(
-    ReadLogFromBxes(),
+    ReadLogFromBxes(use_bytes=True),
     RemainEventsByRegex('(^Procfiler|^Ocel)'),
     RemainOnlyMethodStartEvents(),
     AddStartArtificialEvents(),
     DiscoverRootSequenceGraph(root_sequence_kind=RootSequenceKind.FindBest,
                               merge_sequences_of_events=False),
-    AnnotateGraphWithOCEL()
+    AnnotateGraphWithOCEL(),
+    AssertCorrectOcelAnnotation()
   ))
 
 
@@ -21,9 +35,8 @@ def _execute_ocel_annotation_test(log_name: str, pipeline: Pipeline):
     software_data_config = f.read()
 
   result = _execute_pipeline(pipeline, {
-    'path': StringContextValue(get_ocel_log_path(log_name)),
+    'bytes': BytesContextValue(read_file_bytes(get_ocel_log_path(log_name))),
     'software_data_extraction_config': JsonContextValue(software_data_config)
   })
 
   assert_success_pipeline_final_result(result)
-  print(result)
