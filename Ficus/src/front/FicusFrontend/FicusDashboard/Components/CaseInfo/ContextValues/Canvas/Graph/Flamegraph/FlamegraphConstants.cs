93335d50e10aecd523818ad122e832e12a39f7c6
aerooneqq <aerooneq@yandex.ru>
2025-11-05 15:57:46 +0300

    Adjust font sizes and node size

--- Ficus/src/front/FicusFrontend/FicusDashboard/Components/CaseInfo/ContextValues/Canvas/Graph/Flamegraph/FlamegraphConstants.cs
+++ Ficus/src/front/FicusFrontend/FicusDashboard/Components/CaseInfo/ContextValues/Canvas/Graph/Flamegraph/FlamegraphConstants.cs
@@ -3,6 +3,6 @@
 internal static class FlamegraphConstants
 {
   public const int MainDimPx = 35;
-  public const int NodeWidthPx = 30;
+  public const int NodeWidthPx = 65;
   public const int EdgeWidthPx = 40;
 }
