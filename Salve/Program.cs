ade9743ca84fde5c251c853d785f3c642b329cef
aerooneqq <aerooneq@yandex.ru>
2026-01-09 13:06:22 +0300

    Added dbscan

--- Salve/Program.cs
+++ Salve/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel;
 using System.Diagnostics;
+using System.Numerics;
 using System.Text.RegularExpressions;
 using Bxes.Models.Domain;
 using Bxes.Models.Domain.Values;
 using Bxes.Utils;
 using Bxes.Writer.Stream;
+using Dbscan;
 using JetBrains.Annotations;
 using Spectre.Console;
 using Spectre.Console.Cli;
@@ -127,7 +129,75 @@ internal interface ILogsProcessor : IDisposable
 
 internal partial class RustcLogsParser(string outputPath) : ILogsProcessor
 {
-  private readonly record struct Event(string Message, string Group);
+  private record Event(string Message, string Group) : IPointData
+  {
+    public Point Point => default;
+  }
+
+  private class EventsIndex(List<Event> events) : ISpatialIndex<PointInfo<Event>>
+  {
+    private readonly Dictionary<string, List<PointInfo<Event>>> myEventsByGroups =
+      events
+        .GroupBy(e => e.Group)
+        .ToDictionary(e => e.Key, e => e.Select(evt => new PointInfo<Event>(evt)).ToList());
+
+
+    public IReadOnlyList<PointInfo<Event>> Search() => myEventsByGroups.Values.SelectMany(v => v).ToList();
+
+    public IReadOnlyList<PointInfo<Event>> Search(in IPointData p, double epsilon)
+    {
+      var point = (PointInfo<Event>)p;
+      var result = new List<PointInfo<Event>>();
+
+      foreach (var evt in myEventsByGroups[point.Item.Group])
+      {
+        if (ReferenceEquals(evt, point))
+        {
+          continue;
+        }
+
+        var distance = CalculateEditDistance(point.Item.Message.AsSpan(), evt.Item.Message.AsSpan());
+        if (distance <= epsilon)
+        {
+          result.Add(evt);
+        }
+      }
+
+      return result;
+    }
+
+    private static int CalculateEditDistance<T>(ReadOnlySpan<T> first, ReadOnlySpan<T> second) where T : IEqualityOperators<T, T, bool>
+    {
+      if (first.Length == 0) return second.Length;
+      if (second.Length == 0) return first.Length;
+
+      var current = 1;
+      var previous = 0;
+
+      var r = new int[2, second.Length + 1];
+      for (var i = 0; i <= second.Length; i++)
+      {
+        r[previous, i] = i;
+      }
+
+      for (var i = 0; i < first.Length; i++)
+      {
+        r[current, 0] = i + 1;
+        for (var j = 1; j <= second.Length; j++)
+        {
+          var cost = (second[j - 1] == first[i]) ? 0 : 1;
+          r[current, j] = Min(r[previous, j] + 1, r[current, j - 1] + 1, r[previous, j - 1] + cost);
+        }
+
+        previous = (previous + 1) % 2;
+        current = (current + 1) % 2;
+      }
+
+      return r[previous, second.Length];
+    }
+
+    private static int Min(int e1, int e2, int e3) => Math.Min(Math.Min(e1, e2), e3);
+  }
 
 
   [GeneratedRegex("rustc_([a-z])+::([a-z])+")]
@@ -184,6 +254,18 @@ internal partial class RustcLogsParser(string outputPath) : ILogsProcessor
   public void Dispose()
   {
     using var _ = myLock.EnterScope();
+
+    var clusters = Dbscan.Dbscan.CalculateClusters(new EventsIndex(myEvents), 3, 1);
+    foreach (var cluster in clusters.Clusters)
+    {
+      AnsiConsole.MarkupLine("[blue]CLUSTER[/]");
+      foreach (var obj in cluster.Objects)
+      {
+        AnsiConsole.WriteLine(obj.Message);
+      }
+
+      AnsiConsole.WriteLine();
+    }
 
     foreach (var @event in myEvents)
     {
