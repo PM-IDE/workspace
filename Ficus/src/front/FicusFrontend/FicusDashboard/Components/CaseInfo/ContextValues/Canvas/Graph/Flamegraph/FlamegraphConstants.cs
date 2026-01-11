a243a565e84b82095ed532849a7e341d4ef1b478
aerooneqq <aerooneq@yandex.ru>
2026-01-11 14:37:28 +0300

    Add tooltip for nodes in flamegraph

--- Ficus/src/front/FicusFrontend/FicusDashboard/Components/CaseInfo/ContextValues/Canvas/Graph/Flamegraph/FlamegraphConstants.cs
+++ Ficus/src/front/FicusFrontend/FicusDashboard/Components/CaseInfo/ContextValues/Canvas/Graph/Flamegraph/FlamegraphConstants.cs
@@ -5,4 +5,5 @@ internal static class FlamegraphConstants
   public const int MainDimPx = 35;
   public const int NodeMinSecondDimPx = 20;
   public const int EdgeMinSecondDimPx = 40;
+  public const int NodeMaxTextMainDimPx = 250;
 }
