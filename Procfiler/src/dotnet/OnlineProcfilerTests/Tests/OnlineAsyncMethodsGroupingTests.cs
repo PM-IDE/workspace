145f23f7bac8e82bddfa190d923beddcb7859f8d
aerooneqq <aerooneQ@yandex.ru>
2026-05-02 11:54:10 +0300

    Fix OnlineProcfiler async methods grouping test

--- Procfiler/src/dotnet/OnlineProcfilerTests/Tests/OnlineAsyncMethodsGroupingTests.cs
+++ Procfiler/src/dotnet/OnlineProcfilerTests/Tests/OnlineAsyncMethodsGroupingTests.cs
@@ -25,6 +25,17 @@ public class TestAsyncMethodsHandler : IEventPipeStreamEventHandler
 [NonParallelizable]
 public class OnlineAsyncMethodsGroupingTests : OnlineProcfilerMethodsTest
 {
+  private static IEnumerable<KnownSolution> ourAsyncSolutions =
+  [
+    KnownSolution.SimpleAsyncAwait,
+    KnownSolution.NotSimpleAsyncAwait,
+    KnownSolution.AsyncAwait,
+    KnownSolution.AsyncDisposable,
+    KnownSolution.AwaitForeach,
+    KnownSolution.AsyncAwaitTaskFactoryNew
+  ];
+
+
   private readonly TestAsyncMethodsHandler myHandler = new();
 
   protected override IEnumerable<IEventPipeStreamEventHandler> HandlersToRegister =>
@@ -35,24 +46,8 @@ public class OnlineAsyncMethodsGroupingTests : OnlineProcfilerMethodsTest
   protected override string? Prefix => null;
 
 
-  [Test]
-  public void SimpleAsyncAwait() => Execute(() => DoExecuteTest(KnownSolution.SimpleAsyncAwait));
-
-  [Test]
-  public void NotSimpleAsyncAwait() => Execute(() => DoExecuteTest(KnownSolution.NotSimpleAsyncAwait));
-
-  [Test]
-  public void AsyncAwait() => Execute(() => DoExecuteTest(KnownSolution.AsyncAwait));
-
-  [Test]
-  public void AsyncDisposable() => Execute(() => DoExecuteTest(KnownSolution.AsyncDisposable));
-
-  [Test]
-  public void AwaitForeach() => Execute(() => DoExecuteTest(KnownSolution.AwaitForeach));
-
-  [Test]
-  public void AsyncAwaitTaskFactoryNew() => Execute(() => DoExecuteTest(KnownSolution.AsyncAwaitTaskFactoryNew));
-
+  [TestCaseSource(nameof(ourAsyncSolutions))]
+  public void DoTest(KnownSolution solution) => Execute(() => DoExecuteTest(solution));
 
   protected override Dictionary<string, List<List<EventRecordWithMetadata>>> GetLoggedMethods(ISharedEventPipeStreamData data) =>
     myHandler.RecordedStateMachineTraces;
