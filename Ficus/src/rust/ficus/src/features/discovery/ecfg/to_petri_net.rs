1240b33670e44b3bb7705db207bb39f3b57a57af
aerooneqq <aerooneq@yandex.ru>
2026-09-01 21:38:31 +0300

    Better silent transitions

--- Ficus/src/rust/ficus/src/features/discovery/ecfg/to_petri_net.rs
+++ Ficus/src/rust/ficus/src/features/discovery/ecfg/to_petri_net.rs
@@ -1,28 +1,13 @@
 ﻿use crate::{
-  features::{
-    discovery::petri_net::{petri_net::DefaultPetriNet, place::Place, transition::Transition},
-    mutations::mutations::ARTIFICIAL_START_EVENT_NAME,
-  },
+  features::discovery::petri_net::{petri_net::DefaultPetriNet, place::Place, transition::Transition},
   utils::graph::graph::DefaultGraph,
 };
-use std::{
-  collections::{HashMap, VecDeque},
-  sync::Arc,
-};
-use std::fmt::format;
+use std::{collections::HashMap, sync::Arc};
 
 pub fn convert_to_petri_net(graph: &DefaultGraph) -> Result<DefaultPetriNet, ()> {
   if graph.all_nodes().is_empty() {
     return Ok(Default::default());
   }
-
-  let start_node = graph
-    .nodes
-    .iter()
-    .find(|n| graph.incoming_edges(n.0).is_empty())
-    .map(|n| n.0)
-    .copied()
-    .expect("must contain start node");
 
   let mut petri_net = DefaultPetriNet::default();
 
@@ -31,7 +16,7 @@ pub fn convert_to_petri_net(graph: &DefaultGraph) -> Result<DefaultPetriNet, ()>
 
   for node in graph.all_nodes() {
     let name = Arc::clone(node.data.as_ref().expect("must have name for all transitions"));
-    let t_id = petri_net.add_transition(Transition::empty(name.clone(), false, None));
+    let t_id = petri_net.add_transition(Transition::empty(name.clone(), Some(name.clone())));
 
     let in_place = petri_net.add_place(Place::with_name(format!("{next_place_id}")));
     let out_place = petri_net.add_place(Place::with_name(format!("{}", next_place_id + 1)));
@@ -43,18 +28,14 @@ pub fn convert_to_petri_net(graph: &DefaultGraph) -> Result<DefaultPetriNet, ()>
     next_place_id += 2;
   }
 
-  let mut s_t_idx = 0;
   for edge in graph.all_edges() {
     let from_place = nodes_data[edge.from_node()].1;
     let to_place = nodes_data[edge.to_node()].0;
 
-    let name = Arc::from(format!("{s_t_idx}"));
-    let s_t = petri_net.add_transition(Transition::empty(name, true, None));
+    let s_t = petri_net.add_transition(Transition::silent());
 
     petri_net.connect_place_to_transition(&from_place, &s_t, None);
     petri_net.connect_transition_to_place(&s_t, &to_place, None);
-
-    s_t_idx += 1;
   }
 
   Ok(petri_net)
